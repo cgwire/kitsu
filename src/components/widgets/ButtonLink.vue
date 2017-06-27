1765bcc3fd447464d935abf762e726a570dc9c4b
Frank Rousseau <frank.rousseau@free.fr>
2017-06-27 16:18:16 +0200

    Add widget: button that leads to a new route

---
+++ src/components/widgets/ButtonLink.vue
@@ -0,0 +1,27 @@
+<template>
+<router-link
+  class="button"
+  :to="path"
+>
+  {{ text }}
+</router-link>
+</template>
+
+<script>
+export default {
+  name: 'button-link',
+  props: {
+    text: {
+      default: 'button',
+      type: String
+    },
+    path: {
+      default: '',
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
