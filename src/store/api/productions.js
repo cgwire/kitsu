c1d3187e56f4c0b19f5bef474f4d039d6d3b874b
Frank Rousseau <frank.rousseau@free.fr>
2017-06-27 16:23:28 +0200

    Add helpers to manage production data through API

--- src/store/api/productions.js
+++ src/store/api/productions.js
@@ -7,5 +7,47 @@ export default {
       .end((err, res) => {
         callback(err, res.body)
       })
+  },
+
+  getProductionStatus (callback) {
+    superagent
+      .get('/api/data/project_status')
+      .end((err, res) => {
+        callback(err, res.body)
+      })
+  },
+
+  newProduction (production, callback) {
+    const data = {
+      name: production.name,
+      project_status_id: production.project_status_id
+    }
+    superagent
+      .post(`/api/data/projects/`)
+      .send(data)
+      .end((err, res) => {
+        callback(err, res.body)
+      })
+  },
+
+  updateProduction (production, callback) {
+    const data = {
+      name: production.name,
+      project_status_id: production.project_status_id
+    }
+    superagent
+      .put(`/api/data/projects/${production.id}`)
+      .send(data)
+      .end((err, res) => {
+        callback(err, res.body)
+      })
+  },
+
+  deleteProduction (production, callback) {
+    superagent
+      .del(`/api/data/projects/${production.id}`)
+      .end((err, res) => {
+        callback(err, res.body)
+      })
   }
 }
