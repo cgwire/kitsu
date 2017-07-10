13b44aa0abe16a03ce019210c42ed26ee8f924b8
Frank Rousseau <frank.rousseau@free.fr>
2017-07-10 10:10:24 +0200

    Refactor color field

--- src/components/widgets/ColorField.vue
+++ src/components/widgets/ColorField.vue
@@ -2,8 +2,20 @@
 <div class="field">
   <label class="label">{{ label }}</label>
   <p class="control">
-    <color-picker :value="value" @input="updateValue">
-    </color-picker>
+    <div
+      :class="{
+        color: true,
+        selected: selectedIndex == index
+      }"
+      :ref="'color-' + index"
+      v-for="(color, index) in colors"
+      @click="colorChanged(color, index)"
+    >
+      <span
+        :style="{ background: color }"
+      >
+      </span>
+    </div>
   </p>
 </div>
 </template>
@@ -34,9 +46,26 @@ export default {
   },
   data () {
     return {
-      colors: {
-        hex: '#194d33'
-      }
+      colors: [
+        '#999999',
+        '#8D6E63',
+        '#43A047',
+        '#7CB342',
+        '#009688',
+        '#9CCC65',
+        '#AFB42B',
+        '#F9A825',
+        '#F57C00',
+        '#ff5252',
+        '#F06292',
+        '#AB47BC',
+        '#5C6BC0',
+        '#039BE5',
+        '#26C6DA',
+        '#64B5F6',
+        '#78909C'
+      ],
+      selectedIndex: -1
     }
   },
   components: {
@@ -49,15 +78,30 @@ export default {
   methods: {
     ...mapActions([
     ]),
-    updateValue (color) {
-      this.$emit('input', color.hex)
+    colorChanged (color, index) {
+      this.selectedIndex = index
+      this.$emit('input', color)
     }
   }
 }
 </script>
 <style>
-.vue-color__compact {
-  box-shadow: none;
-  width: 250px;
+.color {
+  display: inline-block;
+  cursor: pointer;
+  border: 3px solid transparent;
+  padding: 3px;
+  width: 52px;
+  height: 52px;
+}
+
+.color span {
+  display: inline-block;
+  width: 40px;
+  height: 40px;
+}
+
+.selected {
+  border: 3px solid #67BE4B;
 }
 </style>
