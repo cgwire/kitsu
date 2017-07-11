1497ff86750d346196c2ee089dc9e44fb2ea400a
Frank Rousseau <frank.rousseau@free.fr>
2017-07-11 19:18:51 +0200

    Sort assets properly

--- src/store/modules/assets.js
+++ src/store/modules/assets.js
@@ -23,17 +23,18 @@ import {
 
 const sortAssets = (assets) => {
   return assets.sort((a, b) => {
-    if (a.project_type_name === b.project_type_name) {
-      return a.name.localeCompare(b.name)
+    if (a.project_name !== b.project_name) {
+      return a.project_name.localeCompare(b.project_name)
+    } else if (a.asset_type_name !== b.asset_type_name) {
+      return a.asset_type_name.localeCompare(b.asset_type_name)
     } else {
-      return -1 * a.project_type_name.localeCompare(b.project_type_name)
+      return a.name.localeCompare(b.name)
     }
   })
 }
 
 const state = {
   assets: [],
-  assetType: [],
   isAssetsLoading: false,
   isAssetsLoadingError: false,
 
