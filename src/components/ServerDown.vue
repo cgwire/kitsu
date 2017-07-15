c77df9c0b73c7968ab02ec31622de35d3d6cd86f
Frank Rousseau <frank.rousseau@free.fr>
2017-07-15 19:58:27 +0200

    Improve server down page behavior
    If server is back it redirects to redirection given inside the query

--- src/components/ServerDown.vue
+++ src/components/ServerDown.vue
@@ -9,16 +9,27 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import auth from '../lib/auth'
 
 export default {
   name: 'server-down',
   computed: {
     ...mapGetters([
+      'isAuthenticated',
+      'user'
     ])
   },
   methods: {
     ...mapActions([
     ])
+  },
+  mounted () {
+    auth.isServerLoggedIn((err) => {
+      const target = this.$store.state.route.query.redirect
+      if (!err) {
+        this.$router.push(target)
+      }
+    })
   }
 }
 </script>
