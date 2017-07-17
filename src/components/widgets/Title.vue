dcaed231cb082fd7ec7f3586d40d6eb64bf14a0c
Frank Rousseau <frank.rousseau@free.fr>
2017-07-17 18:31:28 +0200

    Add widget for titles

---
+++ src/components/widgets/Title.vue
@@ -0,0 +1,18 @@
+<template>
+<h1 class="title">{{ text }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'title',
+  props: {
+    text: {
+      default: 'button',
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
