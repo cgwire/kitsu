ed3b85496e37f877217c54017904381452a7b6c5
Frank Rousseau <frank.rousseau@free.fr>
2017-07-17 17:56:49 +0200

    Add button component for anchor links

---
+++ src/components/widgets/ButtonHrefLink.vue
@@ -0,0 +1,36 @@
+<template>
+<a
+  class="button"
+  :href="path"
+>
+  <span class="icon is-small" v-if="icon.length > 0">
+    <i :class="'fa ' + icon"></i>
+  </span>
+  <span class="text is-hidden-touch">
+    {{ text }}
+  </span>
+</a>
+</template>
+
+<script>
+export default {
+  name: 'button-href-link',
+  props: {
+    text: {
+      default: 'button',
+      type: String
+    },
+    path: {
+      default: '',
+      type: String
+    },
+    icon: {
+      default: '',
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
