366ee56cb10cfdbfacc267b0fbf316b8ceaea2d7
Frank Rousseau <frank.rousseau@free.fr>
2017-07-24 19:30:47 +0200

    Add sorting library

---
+++ src/lib/sorting.js
@@ -0,0 +1,23 @@
+export const sortAssets = (assets) => {
+  return assets.sort((a, b) => {
+    if (a.project_name !== b.project_name) {
+      return a.project_name.localeCompare(b.project_name)
+    } else if (a.asset_type_name !== b.asset_type_name) {
+      return a.asset_type_name.localeCompare(b.asset_type_name)
+    } else {
+      return a.name.localeCompare(b.name)
+    }
+  })
+}
+
+export const sortShots = (shots) => {
+  return shots.sort((a, b) => {
+    if (a.project_name !== b.project_name) {
+      return a.project_name.localeCompare(b.project_name)
+    } else if (a.shot_type_name !== b.shot_type_name) {
+      return a.shot_type_name.localeCompare(b.shot_type_name)
+    } else {
+      return a.name.localeCompare(b.name)
+    }
+  })
+}
