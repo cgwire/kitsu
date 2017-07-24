47ca5e7b4d9e9caceb3536743e122f8737a29158
Frank Rousseau <frank.rousseau@free.fr>
2017-07-24 19:29:50 +0200

    Remove useless parameters for lang.setLocale

--- src/lib/lang.js
+++ src/lib/lang.js
@@ -2,7 +2,7 @@ import store from '../store'
 import i18n from '../i18n'
 
 const lang = {
-  setLocale (to, from, next) {
+  setLocale () {
     i18n.locale = store.state.user.user.locale.substring(0, 2)
   }
 }
