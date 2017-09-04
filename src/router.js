693bd987b95bcc980ea03705e8908c23666ae65a
Frank Rousseau <frank.rousseau@free.fr>
2017-09-04 14:20:38 +0200

    Allow back button to remember the scroll position

--- src/router.js
+++ src/router.js
@@ -3,6 +3,16 @@ import VueRouter from 'vue-router'
 import { routes } from './routes'
 Vue.use(VueRouter)
 
+const scrollBehavior = (to, from, savedPosition) => {
+  if (savedPosition) {
+    return savedPosition
+  } else {
+    return {x: 0, y: 0}
+  }
+}
+
 export default new VueRouter({
+  mode: 'history',
+  scrollBehavior,
   routes
 })
