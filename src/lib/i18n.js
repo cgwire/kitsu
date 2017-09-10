f2f032fa935de954d8017ded34d8d06ad67cd650
Frank Rousseau <frank.rousseau@free.fr>
2017-09-10 17:14:48 +0200

    Put i18n module in lib directory

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -1,8 +1,9 @@
 // Localisation
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
+import locales from '../locales'
+
 Vue.use(VueI18n)
-import locales from './locales'
 
 const i18n = new VueI18n({
   locale: 'en',
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import { sync } from 'vuex-router-sync'
 
-import i18n from './i18n'
+import i18n from './lib/i18n'
 import router from './router'
 import store from './store'
 import App from './App'
