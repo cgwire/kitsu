7844d027387cf3038b6de8dc68c2a6d6c7b79505
Frank Rousseau <frank.rousseau@free.fr>
2017-09-28 09:21:01 +0200

    Fix asset and shot sorting

--- src/lib/sorting.js
+++ src/lib/sorting.js
@@ -1,29 +1,21 @@
+import firstBy from 'thenby'
+
 export const sortAssets = (assets) => {
-  return assets.sort((a, b) => {
-    if (a.canceled !== b.canceled) {
-      return a.canceled ? 1 : -1
-    } if (a.project_name !== b.project_name) {
-      return a.project_name.localeCompare(b.project_name)
-    } else if (a.asset_type_name !== b.asset_type_name) {
-      return a.asset_type_name.localeCompare(b.asset_type_name)
-    } else {
-      return a.name.localeCompare(b.name)
-    }
-  })
+  return assets.sort(
+    firstBy('canceled')
+      .thenBy((a, b) => a.project_name.localeCompare(b.project_name))
+      .thenBy((a, b) => a.asset_type_name.localeCompare(b.asset_type_name))
+      .thenBy((a, b) => a.name.localeCompare(b.name))
+  )
 }
 
 export const sortShots = (shots) => {
-  return shots.sort((a, b) => {
-    if (a.canceled !== b.canceled) {
-      return a.canceled ? 1 : -1
-    } if (a.project_name !== b.project_name) {
-      return a.project_name.localeCompare(b.project_name)
-    } else if (a.sequence_name !== b.sequence_name) {
-      return a.sequence_name.localeCompare(b.sequence_name)
-    } else {
-      return a.name.localeCompare(b.name)
-    }
-  })
+  return shots.sort(
+    firstBy('canceled')
+      .thenBy((a, b) => a.project_name.localeCompare(b.project_name))
+      .thenBy((a, b) => a.sequence_name.localeCompare(b.sequence_name))
+      .thenBy((a, b) => a.name.localeCompare(b.name))
+  )
 }
 
 export const sortProductions = (productions) => {
