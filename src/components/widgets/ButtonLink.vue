e4f559da70ed8beb0f2afe7c849b69143dfc0e59
Frank Rousseau <frank.rousseau@free.fr>
2017-10-04 11:40:07 +0200

    Allow to use feather icon in buttons

--- src/components/widgets/ButtonLink.vue
+++ src/components/widgets/ButtonLink.vue
@@ -3,18 +3,30 @@
   class="button"
   :to="path"
 >
-  <span class="icon is-small" v-if="icon.length > 0">
+
+  <span class="icon is-small" v-if="icon.length > 0 && icon.startsWith('fa')">
     <i :class="'fa ' + icon"></i>
   </span>
-   <span class="text is-hidden-touch" v-if="text.length > 0">
+  <plus-icon class="icon is-small" v-if="icon === 'plus'"></plus-icon>
+  <download-icon class="icon is-small" v-if="icon === 'download'"></download-icon>
+  <upload-icon class="icon is-small" v-if="icon === 'upload'"></upload-icon>
+
+  <span class="text is-hidden-touch" v-if="text.length > 0">
     {{ text }}
   </span>
 </router-link>
 </template>
 
 <script>
+import { PlusIcon, DownloadIcon, UploadIcon } from 'vue-feather-icons'
+
 export default {
   name: 'button-link',
+  components: {
+    PlusIcon,
+    DownloadIcon,
+    UploadIcon
+  },
   props: {
     text: {
       default: '',
