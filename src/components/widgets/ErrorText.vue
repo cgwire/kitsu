d9963059b376e0df494779974989b4185804263b
Frank Rousseau <frank.rousseau@free.fr>
2017-10-17 17:36:23 +0200

    Extract error text from breakdown component
    Make a component from it

---
+++ src/components/widgets/ErrorText.vue
@@ -0,0 +1,29 @@
+<template>
+<p :class="{
+  'error': true,
+  'has-text-right': alignRight,
+  'is-hidden': hidden
+}">
+  {{ text }}
+</p>
+</template>
+
+<script>
+export default {
+  name: 'error-text',
+  props: {
+    text: {
+      default: '',
+      type: String
+    },
+    hidden: {
+      default: false,
+      type: Boolean
+    },
+    alignRight: {
+      default: false,
+      type: Boolean
+    }
+  }
+}
+</script>
