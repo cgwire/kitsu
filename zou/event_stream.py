dbcd40d339968ece3ddb94d5fe9f6ddaab881967
Frank Rousseau <frank.rousseau@free.fr>
2017-10-18 20:56:08 +0200

    Fix default port for Redis in event stream

--- zou/event_stream.py
+++ zou/event_stream.py
@@ -5,7 +5,7 @@ from flask_sse import sse
 
 app = Flask(__name__)
 redis_host =  os.environ.get("KV_HOST", "localhost")
-redis_port =  os.environ.get("KV_PORT", "5379")
+redis_port =  os.environ.get("KV_PORT", "6379")
 redis_url = "redis://%s:%s/2" % (redis_host, redis_port)
 
 app.config["REDIS_URL"] = redis_url
