557e94f9407c0f2d3d6b8faba70209a3d13f3280
Frank Rousseau <frank.rousseau@free.fr>
2017-10-18 18:14:14 +0200

    Use right env variable to build redis url
    It is for the events stream daemon.

--- zou/event_stream.py
+++ zou/event_stream.py
@@ -8,5 +8,6 @@ redis_host =  os.environ.get("KV_HOST", "localhost")
 redis_port =  os.environ.get("KV_PORT", "6379")
 redis_url = "redis://%s:%s/2" % (redis_host, redis_port)
 
+
 app.config["REDIS_URL"] = redis_url
 app.register_blueprint(sse, url_prefix='/events')
