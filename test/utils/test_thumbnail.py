b89351f501ea727147fd366fd9e55fbd05aeead6
Frank Rousseau <frank.rousseau@free.fr>
2017-11-01 09:58:45 +0100

    Add utils to better manage preview pictures
    * Allow to keep original size of a file
    * Create multiple variants in a single function call
    * Get quickly the folder and file names of a given preview

--- test/utils/test_thumbnail.py
+++ test/utils/test_thumbnail.py
@@ -1,6 +1,5 @@
 import unittest
 import os
-import shutil
 
 from PIL import Image
 
@@ -31,6 +30,8 @@ class ThumbnailTestCase(unittest.TestCase):
         super(ThumbnailTestCase, self).tearDown()
         folder_name = thumbnail.get_folder_name("shots")
         fs.rm_rf(folder_name)
+        fs.rm_rf("test/tmp")
+        fs.rm_rf("thumbnails")
 
     def test_get_file_name(self):
         file_name = thumbnail.get_file_name("instance-id")
@@ -41,8 +42,16 @@ class ThumbnailTestCase(unittest.TestCase):
         expected_result = os.path.join(app.config["THUMBNAIL_FOLDER"], "shots")
         self.assertEqual(folder_name, expected_result)
 
-    def test_get_full_path(self):
-        full_path = thumbnail.get_full_path("shots", "instance-id")
+    def test_get_preview_folder_name(self):
+        path = thumbnail.get_preview_folder_name("originals", "123413-12312")
+        self.assertEquals(
+            path,
+            "/media/storage/frankrousseau/Projets/"
+            "cg-wire/products/zou/thumbnails/preview-files/originals/123"
+        )
+
+    def test_get_file_path(self):
+        full_path = thumbnail.get_file_path("shots", "instance-id")
         expected_result = os.path.join(
             app.config["THUMBNAIL_FOLDER"],
             "shots",
@@ -50,18 +59,24 @@ class ThumbnailTestCase(unittest.TestCase):
         )
         self.assertEqual(full_path, expected_result)
 
-    def test_create_type_folder(self):
-        thumbnail.create_type_folder("shots")
+    def test_create_folder(self):
+        thumbnail.create_folder("shots")
         folder_name = thumbnail.get_folder_name("shots")
         self.assertTrue(os.path.exists(folder_name))
 
     def test_turn_into_thumbnail(self):
         file_path_fixture = self.get_fixture_file_path("thumbnails/th01.png")
-        full_path = thumbnail.get_full_path("shots", "instance-id")
-        thumbnail.create_type_folder("shots")
-        shutil.copyfile(file_path_fixture, full_path)
+        full_path = thumbnail.get_file_path("shots", "instance-id")
+        thumbnail.create_folder("shots")
+        fs.copyfile(file_path_fixture, full_path)
 
         thumbnail.turn_into_thumbnail(full_path)
+        im = Image.open(full_path)
+        (width, height) = im.size
+        self.assertEqual(width, 180)
+        self.assertEqual(height, 101)
+
+        thumbnail.turn_into_thumbnail(full_path, thumbnail.RECTANGLE_SIZE)
         im = Image.open(full_path)
         (width, height) = im.size
         self.assertEqual(width, 150)
@@ -75,7 +90,7 @@ class ThumbnailTestCase(unittest.TestCase):
         )
         full_path = thumbnail.save_file("shots", "instance-id", th_file)
 
-        thumbnail.turn_into_thumbnail(full_path)
+        thumbnail.turn_into_thumbnail(full_path, thumbnail.RECTANGLE_SIZE)
         im = Image.open(full_path)
         (width, height) = im.size
         self.assertEqual(width, 150)
@@ -83,6 +98,74 @@ class ThumbnailTestCase(unittest.TestCase):
 
     def test_url_path(self):
         url_path = thumbnail.url_path("shots", "instance-id")
-        self.assertEqual(url_path, "thumbnails/shots/instance-id.png")
+        self.assertEqual(
+            url_path,
+            "pictures/thumbnails/shots/instance-id.png"
+        )
         url_path = thumbnail.url_path("working_files", "instance-id")
-        self.assertEqual(url_path, "thumbnails/working-files/instance-id.png")
+        self.assertEqual(
+            url_path,
+            "pictures/thumbnails/working-files/instance-id.png"
+        )
+
+    def test_flat(self):
+        flatten_tupple = thumbnail.flat(1.2, 3.1, 4.2)
+        self.assertEquals(flatten_tupple, (1, 3, 4))
+
+    def test_get_image_size(self):
+        file_path_fixture = self.get_fixture_file_path("thumbnails/th01.png")
+        size = thumbnail.get_image_size(file_path_fixture)
+        self.assertEquals(size, (180, 101))
+
+    def test_get_full_size_from_width(self):
+        file_path_fixture = self.get_fixture_file_path("thumbnails/th01.png")
+        im = Image.open(file_path_fixture)
+        size = thumbnail.get_full_size_from_width(im, 1200)
+        self.assertEquals(size, (1200, 674))
+
+    def test_prepare_image_for_thumbnail(self):
+        file_path_fixture = self.get_fixture_file_path("thumbnails/th01.png")
+        im = Image.open(file_path_fixture)
+        im = thumbnail.prepare_image_for_thumbnail(im, thumbnail.SQUARE_SIZE)
+        self.assertEquals(im.size, (100, 101))
+
+    def test_generate_preview_variants(self):
+        preview_id = "123413-12312"
+        file_path_fixture = self.get_fixture_file_path("thumbnails/th01.png")
+        file_name = thumbnail.get_file_name(preview_id)
+        folder_path = thumbnail.get_preview_folder_name("originals", preview_id)
+        fs.mkdir_p(folder_path)
+        fs.copyfile(file_path_fixture, os.path.join(folder_path, file_name))
+        thumbnail.generate_preview_variants(preview_id)
+
+        file_path = thumbnail.get_preview_file_path(
+            "previews", preview_id)
+        self.assertTrue(os.path.exists(file_path))
+        self.assertTrue(Image.open(file_path).size, thumbnail.PREVIEW_SIZE)
+
+        folder_path = thumbnail.get_preview_folder_name(
+            "thumbnails", preview_id)
+        self.assertTrue(os.path.exists(file_path))
+        self.assertTrue(Image.open(file_path).size, thumbnail.RECTANGLE_SIZE)
+
+        folder_path = thumbnail.get_preview_folder_name(
+            "thumbnails-square", preview_id)
+        self.assertTrue(os.path.exists(file_path))
+        self.assertTrue(Image.open(file_path).size, thumbnail.SQUARE_SIZE)
+
+    def test_get_preview_url_path(self):
+        preview_id = '123345-12234-121234'
+        path = thumbnail.get_preview_url_path(preview_id)
+        self.assertEquals(
+            path,
+            {
+                "original": "/api/pictures/originals/preview-files/%s.png" %
+                preview_id,
+                "previews": "/api/pictures/previews/preview-files/%s.png" %
+                preview_id,
+                "thumbnail": "/api/pictures/thumbnails/preview-files/%s.png" %
+                preview_id,
+                "thumbnail_square": "/api/pictures/thumbnails-square/preview-files/%s.png" %
+                preview_id,
+            }
+        )
