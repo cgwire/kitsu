9af66cee6103bb1ca8b663da751f032cb9cc867d
Pierre-Louis Bonicoli <pierre-louis.bonicoli@libregerbil.fr>
2017-11-28 13:22:05 +0100

    use 'attr:' directive for version

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python
-from imp import load_source
-from os import path
 from setuptools import setup
 
-
-zou = load_source('version', path.join('zou', '__init__.py'))
-
-setup(version=zou.__version__)
+setup()
