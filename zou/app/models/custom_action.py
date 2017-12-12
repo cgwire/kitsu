490ce27b6e9213cd9200b6fb42e7676af58abd58
Frank Rousseau <frank.rousseau@free.fr>
2017-12-12 16:16:35 +0100

    Add entity type column to actions

--- zou/app/models/custom_action.py
+++ zou/app/models/custom_action.py
@@ -6,3 +6,4 @@ from zou.app.models.base import BaseMixin
 class CustomAction(db.Model, BaseMixin, SerializerMixin):
     name = db.Column(db.String(80), nullable=False)
     url = db.Column(db.String(400))
+    entity_type = db.Column(db.String(40), default="all")
