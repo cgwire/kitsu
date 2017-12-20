2ba5433c4cd144506c821627f575f11e77c73800
Frank Rousseau <frank.rousseau@free.fr>
2017-12-20 11:00:11 +0100

    Use precomputed data to generate person avatars

--- src/components/widgets/PeopleAvatar.vue
+++ src/components/widgets/PeopleAvatar.vue
@@ -1,50 +1,25 @@
 <template>
 <span class="avatar has-text-centered"
    :style="{
-     background: getAvatarColor(person),
+     background: person.color,
      width: (size || 40) +'px',
      height: (size || 40) + 'px',
      'font-size': (fontSize || 18) + 'px'
    }">
    <span>
-     {{ generateAvatar(person) }}
+     {{ person.initials }}
    </span>
 </span>
 </template>
 
 <script>
-import colors from '../../lib/colors.js'
-import { mapGetters, mapActions } from 'vuex'
-
 export default {
   name: 'person-avatar',
   props: [
     'font-size',
     'person',
     'size'
-  ],
-  computed: {
-    ...mapGetters([
-    ])
-  },
-  methods: {
-    ...mapActions([
-    ]),
-    generateAvatar (person) {
-      if (person !== null) {
-        return (person.first_name[0] + person.last_name[0]).toUpperCase()
-      } else {
-        return 'No user logged'
-      }
-    },
-    getAvatarColor (person) {
-      if (person !== null) {
-        return colors.fromString(person.first_name + person.last_name)
-      } else {
-        return '#CCC'
-      }
-    }
-  }
+  ]
 }
 </script>
 
