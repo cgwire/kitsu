f442f426bf4470db28594753cde4ab9aec2fb87b
Frank Rousseau <frank.rousseau@free.fr>
2017-12-26 16:13:34 +0100

    Add string utils to generate next names

---
+++ src/lib/string.js
@@ -0,0 +1,18 @@
+export default {
+  /*
+   * Detect if number are present in the name and generate the next
+   * increment. If there is no number, we consider that a new name
+   * must be written.
+   */
+  generateNextName (name) {
+    var matches = name.match(/\d+$/)
+    if (matches) {
+      const number = matches[0]
+      const rootName = name.substring(0, name.length - number.length)
+      const numberInt = parseInt(number) + 1
+      return rootName + String(numberInt).padStart(number.length, '0')
+    } else {
+      return ''
+    }
+  }
+}
