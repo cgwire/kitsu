ba84bf6e9831d682fbaea3946937dc44d9f5a939
Frank Rousseau <frank.rousseau@free.fr>
2018-01-03 19:22:07 +0100

    Add tests for sorting functions

--- test/store/lib/sorting.spec.js
+++ test/store/lib/sorting.spec.js
@@ -4,11 +4,13 @@ import {
   sortByName,
   sortProductions,
   sortShots,
+  sortTaskTypes,
+  sortTasks,
   sortValidationColumns
 } from '../../../src/lib/sorting'
 
 
-describe('sorting', () => {
+describe('lib/sorting', () => {
 
   beforeEach(() => {
   })
@@ -75,45 +77,45 @@ describe('sorting', () => {
     const entries = [
       {
         canceled: false,
-        project_name: 'Big Buck Bunny',
+        episode_name: 'E01',
         sequence_name: 'SE02',
         name: 'SH03',
-        id: 5
-      },
-      {
-        canceled: false,
-        project_name: 'Big Buck Bunny',
-        sequence_name: 'SE02',
-        name: 'SH02',
         id: 4
       },
       {
         canceled: false,
-        project_name: 'Big Buck Bunny',
+        episode_name: 'E01',
         sequence_name: 'SE02',
-        name: 'SH01',
+        name: 'SH02',
         id: 3
       },
       {
+        canceled: false,
+        episode_name: 'E01',
+        sequence_name: 'SE02',
+        name: 'SH01',
+        id: 2
+      },
+      {
         canceled: true,
-        project_name: 'Agent 327',
-        sequence_name: 'SE01',
+        episode_name: 'E01',
+        sequence_name: 'SE03',
         name: 'SH01',
         id: 6
       },
       {
         canceled: false,
-        project_name: 'Big Buck Bunny',
+        episode_name: 'E01',
         sequence_name: 'SE01',
         name: 'SH01',
-        id: 2
+        id: 1
       },
       {
         canceled: false,
-        project_name: 'Agent 327',
+        episode_name: 'E02',
         sequence_name: 'SE01',
-        name: 'SH02',
-        id: 1
+        name: 'SH01',
+        id: 5
       }
     ]
     let results = sortShots(entries)
@@ -141,6 +143,100 @@ describe('sorting', () => {
     expect(results[0].id).to.equal(1)
     expect(results[1].id).to.equal(2)
     expect(results[2].id).to.equal(3)
+
+    results = sortProductions([])
+    expect(results.length).to.equal(0)
+  })
+
+  it('sortProductions', () => {
+    const entries = [
+      {project_status_name: 'closed', name: 'Big Buck Bunny', id: 3},
+      {project_status_name: 'open', name: 'Cosmos Landromat', id: 2},
+      {project_status_name: 'open', name: 'Agent 327', id: 1}
+    ]
+    let results = sortProductions(entries)
+    expect(results.length).to.equal(3)
+    expect(results[0].id).to.equal(1)
+    expect(results[1].id).to.equal(2)
+    expect(results[2].id).to.equal(3)
+
+    results = sortProductions([])
+    expect(results.length).to.equal(0)
+  })
+
+  it('sortTaskTypes', () => {
+    const entries = [
+      {for_shots: false, priority: 1, name: 'Modeling', id: 2},
+      {for_shots: false, priority: 2, name: 'Setup', id: 5},
+      {for_shots: false, priority: 1, name: 'Modeling Low', id: 4},
+      {for_shots: false, priority: 1, name: 'Modeling Hi', id: 3},
+      {for_shots: true, priority: 1, name: 'Animation', id: 1}
+    ]
+    let results = sortProductions(entries)
+    expect(results.length).to.equal(5)
+    expect(results[0].id).to.equal(1)
+    expect(results[1].id).to.equal(2)
+    expect(results[2].id).to.equal(3)
+    expect(results[3].id).to.equal(4)
+    expect(results[4].id).to.equal(5)
+
+    results = sortProductions([])
+    expect(results.length).to.equal(0)
+  })
+
+  it('sortTasks', () => {
+    const entries = [
+      {
+        project_name: 'Big Buck Bunny',
+        task_type_name: 'Modeling',
+        entity_name: 'Props / Tree',
+        id: 4
+      },
+      {
+        project_name: 'Agent 327',
+        task_type_name: 'Modeling',
+        entity_name: 'Characters / Agent327',
+        id: 1
+      },
+      {
+        project_name: 'Agent 327',
+        task_type_name: 'Setup',
+        entity_name: 'Characters / Agent327',
+        id: 3
+      },
+      {
+        project_name: 'Agent 327',
+        task_type_name: 'Modeling',
+        entity_name: 'Characters / SuperVilain',
+        id: 2
+      }
+    ]
+    let results = sortTasks(entries)
+    expect(results.length).to.equal(4)
+    expect(results[0].id).to.equal(1)
+    expect(results[1].id).to.equal(2)
+    expect(results[2].id).to.equal(3)
+    expect(results[3].id).to.equal(4)
+
+    results = sortProductions([])
+    expect(results.length).to.equal(0)
+  })
+
+  it('sortTaskTypes', () => {
+    const entries = [
+      {for_shots: false, priority: 1, name: 'Modeling', id: 2},
+      {for_shots: false, priority: 2, name: 'Setup', id: 5},
+      {for_shots: false, priority: 1, name: 'Modeling Low', id: 4},
+      {for_shots: false, priority: 1, name: 'Modeling Hi', id: 3},
+      {for_shots: true, priority: 1, name: 'Animation', id: 1}
+    ]
+    let results = sortProductions(entries)
+    expect(results.length).to.equal(5)
+    expect(results[0].id).to.equal(1)
+    expect(results[1].id).to.equal(2)
+    expect(results[2].id).to.equal(3)
+    expect(results[3].id).to.equal(4)
+    expect(results[4].id).to.equal(5)
 
     results = sortProductions([])
     expect(results.length).to.equal(0)
--- test/test-store.js
+++ test/test-store.js
@@ -1,5 +1,6 @@
 import expect from 'chai'
 
+/*
 import './store/login.spec'
 import './store/main.spec'
 import './store/people.spec'
@@ -9,6 +10,7 @@ import './store/tasktypes.spec.js'
 import './store/assettypes.spec.js'
 import './store/customactions.spec.js'
 import './store/breakdown.spec.js'
+*/
 
 import './store/lib/sorting.spec.js'
 import './store/lib/string.spec.js'
