64086acee22cfc2dde2fec9da1ea1b7745ce3d85
Frank Rousseau <frank.rousseau@free.fr>
2018-01-08 17:10:34 +0100

    Remove useless test about model representation

--- tests/misc/test_base_model.py
+++ tests/misc/test_base_model.py
@@ -4,11 +4,6 @@ from tests.base import ApiDBTestCase
 
 class BaseModelTestCase(ApiDBTestCase):
 
-    def test_repr(self):
-        self.generate_fixture_project_status()
-        self.generate_fixture_project()
-        self.assertEqual(str(self.project), "<Project %s>" % self.project.name)
-
     def test_query(self):
         pass
 
