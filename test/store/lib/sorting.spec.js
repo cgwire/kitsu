95c727183a8ab6c22ab43b059bd6340dd0b3926b
Frank Rousseau <frank.rousseau@free.fr>
2018-01-22 13:28:41 +0100

    Fix sorting when shots have no episode

--- test/store/lib/sorting.spec.js
+++ test/store/lib/sorting.spec.js
@@ -132,6 +132,67 @@ describe('lib/sorting', () => {
     expect(results.length).to.equal(0)
   })
 
+  it('sortShots - without episodes', () => {
+    const entries = [
+      {
+        canceled: false,
+        sequence_name: 'SE02',
+        name: 'SH01',
+        id: 4
+      },
+      {
+        canceled: false,
+        sequence_name: 'SE03',
+        name: 'SH02',
+        id: 6
+      },
+      {
+        canceled: false,
+        sequence_name: 'SE01',
+        name: 'SH01',
+        id: 1
+      },
+      {
+        canceled: false,
+        sequence_name: 'SE03',
+        name: 'SH01',
+        id: 5
+      },
+      {
+        canceled: true,
+        sequence_name: 'SE01',
+        name: 'SH04',
+        id: 7
+      },
+      {
+        canceled: false,
+        sequence_name: 'SE01',
+        name: 'SH02',
+        id: 2
+      },
+      {
+        canceled: false,
+        sequence_name: 'SE01',
+        name: 'SH03',
+        id: 3
+      }
+    ]
+    let results = sortShots(entries)
+
+    console.log(results)
+    expect(results.length).to.equal(7)
+    expect(results[0].id).to.equal(1)
+    expect(results[1].id).to.equal(2)
+    expect(results[2].id).to.equal(3)
+    expect(results[3].id).to.equal(4)
+    expect(results[4].id).to.equal(5)
+    expect(results[5].id).to.equal(6)
+    expect(results[6].id).to.equal(7)
+
+    results = sortShots([])
+    expect(results.length).to.equal(0)
+  })
+
   it('sortProductions', () => {
     const entries = [
       {project_status_name: 'closed', name: 'Big Buck Bunny', id: 3},
