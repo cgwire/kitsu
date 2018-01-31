d8d1ea016db1ecda21253b7a1d8126ae51a73295
Frank Rousseau <frank.rousseau@free.fr>
2018-01-31 12:10:07 +0100

    Change base role names

--- src/locales/en.js
+++ src/locales/en.js
@@ -33,8 +33,8 @@ export default {
     },
     role: {
       admin: 'Administrator',
-      manager: 'Manager',
-      user: 'User'
+      manager: 'Supervisor',
+      user: 'CG Artist'
     }
   },
 
