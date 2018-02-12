ed8ae177515515e2b2c10e42274a3afeada41dab
Frank Rousseau <frank.rousseau@free.fr>
2018-02-12 14:15:49 +0100

    Change role locales for French

--- src/locales/fr.js
+++ src/locales/fr.js
@@ -33,8 +33,8 @@ export default {
     },
     role: {
       admin: 'Administrateur',
-      manager: 'Manager',
-      user: 'Utilisateur'
+      manager: 'Superviseur',
+      user: 'Graphiste'
     }
   },
 
