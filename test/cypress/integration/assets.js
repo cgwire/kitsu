db70f1898aabf675ef421426cf9295a947d285fd
ergot <wjose14@hotmail.com>
2018-02-28 11:53:13 +0100

    adding modeling and setup task

--- test/cypress/integration/assets.js
+++ test/cypress/integration/assets.js
@@ -1,4 +1,4 @@
-describe('Creating assets', function () {
+describe('Testing assets page', function () {
   before(function () {
     Cypress.Cookies.defaults({
       whitelist: ['session_id', 'access_token_cookie', 'refresh_token_cookie']
@@ -16,35 +16,55 @@ describe('Creating assets', function () {
     cy.get('aside').contains('Assets').click()
   })
 
-  const types = ['Characters', 'Props']
-  const assetsFixtures = [
-    {
-      type: types[0],
-      asset: 'Agent 327'
-    }, {
-      type: types[0],
-      asset: 'Super Evil'
-    }, {
-      type: types[1],
-      asset: 'Gun'
-    }
-  ]
+  context('Creating assets', function () {
+    const types = ['Characters', 'Props']
+    const assetsFixtures = [
+      {
+        type: types[0],
+        asset: 'Agent 327'
+      }, {
+        type: types[0],
+        asset: 'Super Evil'
+      }, {
+        type: types[1],
+        asset: 'Gun'
+      }
+    ]
 
-  assetsFixtures.forEach(function (asset) {
-    it('click to add assets ', function () {
-      cy.get('.asset-list-header > .level > .level-right > div.level-item').contains('Add an asset').click()
+    assetsFixtures.forEach(function (asset) {
+      it('click to add assets ', function () {
+        cy.get('.asset-list-header > .level > .level-right > div.level-item').contains('Add an asset').click()
+      })
+
+      it('select a type', function () {
+        cy.get('.is-active > .modal-content > .box > form > :nth-child(1) > .control > .select > select').select(asset.type)
+      })
+
+      it('type asset input', function () {
+        cy.get(':nth-child(2) > .control > .input').type(asset.asset)
+      })
+
+      it('sending the form', function () {
+        cy.get('.box > :nth-child(3) > :nth-child(2)').contains('Confirm').click()
+      })
     })
+  })
 
-    it('select a type', function () {
-      cy.get('.is-active > .modal-content > .box > form > :nth-child(1) > .control > .select > select').select(asset.type)
-    })
+  context('Adding tasks', function () {
+    const tasksFixtures = ['Modeling', 'Setup']
 
-    it('type asset input', function () {
-      cy.get(':nth-child(2) > .control > .input').type(asset.asset)
-    })
+    tasksFixtures.forEach(function (task) {
+      it('click on add task', function () {
+        cy.get('thead > tr > .actions > .button').contains('Add tasks').click().wait(500)
+      })
 
-    it('sending the form', function () {
-      cy.get('.box > :nth-child(3) > :nth-child(2)').contains('Confirm').click()
+      it('click a type of task', function () {
+        cy.get('.is-active > .modal-content > .box > form > .field > .control > .select > select').select(task)
+      })
+
+      it('click on confirm', function () {
+        cy.get('.is-active > .modal-content > .box > .has-text-right > .is-primary').contains('Confirm').click().wait(500)
+      })
     })
   })
 })
