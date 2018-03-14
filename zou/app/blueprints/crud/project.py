188bf9c0d277e465d7ad8d3dfa99aa044eab7955
Frank Rousseau <frank.rousseau@free.fr>
2018-03-14 18:25:08 +0100

    Force all new project to have open status

--- zou/app/blueprints/crud/project.py
+++ zou/app/blueprints/crud/project.py
@@ -1,5 +1,5 @@
 from zou.app.models.project import Project
-from zou.app.services import user_service
+from zou.app.services import user_service, projects_service
 
 from .base import BaseModelResource, BaseModelsResource
 
@@ -17,3 +17,7 @@ class ProjectResource(BaseModelResource):
 
     def check_read_permissions(self, project):
         user_service.check_project_access(project["id"])
+
+    def update_data(self, data):
+        open_status = projects_service.get_or_create_open_status()
+        data["open_status_id"] = open_status["id"]
