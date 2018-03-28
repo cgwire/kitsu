7b14c6243729de86ffd2a86587fc32e64620114d
Frank Rousseau <frank.rousseau@free.fr>
2018-03-28 19:24:24 +0200

    Add store functions for playlists
    * Modify functions
    * Preview loading functions

--- src/store/modules/playlists.js
+++ src/store/modules/playlists.js
@@ -1,12 +1,14 @@
 import playlistsApi from '../api/playlists'
-import { sortByName } from '../../lib/sorting'
-
-import productionsStore from './productions'
+import { sortPlaylists } from '../../lib/sorting'
 
 import {
   LOAD_PLAYLISTS_START,
   LOAD_PLAYLISTS_ERROR,
   LOAD_PLAYLISTS_END,
+
+  LOAD_PLAYLIST_START,
+  LOAD_PLAYLIST_ERROR,
+  LOAD_PLAYLIST_END,
 
   EDIT_PLAYLIST_START,
   EDIT_PLAYLIST_ERROR,
@@ -16,43 +18,28 @@ import {
   DELETE_PLAYLIST_ERROR,
   DELETE_PLAYLIST_END,
 
+  CHANGE_PLAYLIST_PREVIEW,
+  ADD_SHOT_TO_PLAYLIST,
+  REMOVE_SHOT_FROM_PLAYLIST,
+  LOAD_SHOT_PREVIEW_FILES_END,
+
   RESET_ALL
 } from '../mutation-types'
 
 const state = {
   playlists: [],
-  playlistMap: {},
-  isPlaylistsLoading: false,
-  isPlaylistsLoadingError: false,
-
-  editPlaylist: {
-    isLoading: false,
-    isError: false
-  },
-
-  deletePlaylist: {
-    isLoading: false,
-    isError: false
-  }
+  playlistMap: {}
 }
 
 const getters = {
   playlists: state => state.playlists,
-  playlistMap: state => state.playlistMap,
-
-  isPlaylistsLoading: state => state.isPlaylistsLoading,
-  isPlaylistsLoadingError: state => state.isPlaylistsLoadingError,
-
-  editPlaylist: state => state.editPlaylist,
-  deletePlaylist: state => state.deletePlaylist
+  playlistMap: state => state.playlistMap
 }
 
 const actions = {
 
-  loadPlaylists ({ commit, state, rootState }, callback) {
-    const currentProduction = productionsStore.getters.currentProduction(
-      rootState.productions
-    )
+  loadPlaylists ({ commit, rootGetters }, callback) {
+    const currentProduction = rootGetters.currentProduction
     commit(LOAD_PLAYLISTS_START)
     playlistsApi.getPlaylists(currentProduction, (err, playlists) => {
       if (err) commit(LOAD_PLAYLISTS_ERROR)
@@ -61,40 +48,75 @@ const actions = {
     })
   },
 
-  newPlaylist ({ commit, state }, { data, callback }) {
+  loadPlaylist ({ commit, rootGetters }, { playlist, callback }) {
+    const currentProduction = rootGetters.currentProduction
+    commit(LOAD_PLAYLIST_START)
+    playlistsApi.getPlaylist(
+      currentProduction, playlist, (err, playlist) => {
+        if (err) commit(LOAD_PLAYLIST_ERROR)
+        else commit(LOAD_PLAYLIST_END, playlist)
+        if (callback) callback(err, playlist)
+      })
+  },
+
+  loadShotPreviewFiles ({ commit }, { playlist, shot, callback }) {
+    playlistsApi.getShotPreviewFiles(shot, (err, previewFiles) => {
+      if (callback) callback(err, previewFiles)
+    })
+  },
+
+  newPlaylist ({ commit }, { data, callback }) {
     commit(EDIT_PLAYLIST_START, data)
     playlistsApi.newPlaylist(data, (err, playlist) => {
-      if (err) {
-        commit(EDIT_PLAYLIST_ERROR)
-      } else {
-        commit(EDIT_PLAYLIST_END, playlist)
-      }
+      if (err) commit(EDIT_PLAYLIST_ERROR)
+      else commit(EDIT_PLAYLIST_END, playlist)
+      if (callback) callback(err, playlist)
+    })
+  },
+
+  editPlaylist ({ commit }, {data, callback}) {
+    commit(EDIT_PLAYLIST_START)
+    playlistsApi.updatePlaylist(data, (err, playlist) => {
+      if (err) commit(EDIT_PLAYLIST_ERROR)
+      else commit(EDIT_PLAYLIST_END, playlist)
       if (callback) callback(err)
     })
   },
 
-  editPlaylist ({ commit, state }, payload) {
-    commit(EDIT_PLAYLIST_START)
-    playlistsApi.updatePlaylist(payload.data, (err, playlist) => {
-      if (err) {
-        commit(EDIT_PLAYLIST_ERROR)
-      } else {
-        commit(EDIT_PLAYLIST_END, playlist)
-      }
-      if (payload.callback) payload.callback(err)
-    })
-  },
-
-  deletePlaylist ({ commit, state }, { playlist, callback }) {
+  deletePlaylist ({ commit }, { playlist, callback }) {
     commit(DELETE_PLAYLIST_START)
     playlistsApi.deletePlaylist(playlist, (err) => {
-      if (err) {
-        commit(DELETE_PLAYLIST_ERROR)
-      } else {
-        commit(DELETE_PLAYLIST_END, playlist)
-      }
+      if (err) commit(DELETE_PLAYLIST_ERROR)
+      else commit(DELETE_PLAYLIST_END, playlist)
       if (callback) callback(err)
     })
+  },
+
+  addShotPreviewToPlaylist (
+    { commit, dispatch }, { playlist, shot, previewFiles, callback }
+  ) {
+    commit(ADD_SHOT_TO_PLAYLIST, {playlist, shot})
+    commit(LOAD_SHOT_PREVIEW_FILES_END, { playlist, shot, previewFiles })
+    dispatch('editPlaylist', { data: playlist, callback })
+  },
+
+  removeShotPreviewFromPlaylist (
+    { commit, dispatch }, {playlist, shot, callback}
+  ) {
+    commit(REMOVE_SHOT_FROM_PLAYLIST, { playlist, shot })
+    dispatch('editPlaylist', {data: playlist, callback})
+  },
+
+  changePlaylistPreview (
+    { commit, dispatch },
+    { playlist, shot, previewFileId, callback }
+  ) {
+    commit(CHANGE_PLAYLIST_PREVIEW, {
+      playlist,
+      shotId: shot.id,
+      previewFileId
+    })
+    dispatch('editPlaylist', {data: playlist, callback})
   }
 }
 
@@ -107,11 +129,21 @@ const mutations = {
 
   [LOAD_PLAYLISTS_END] (state, playlists) {
     state.playlists = playlists
-    state.playlists = sortByName(state.playlists)
+    state.playlists = sortPlaylists(state.playlists)
     state.playlistMap = {}
     playlists.forEach((playlist) => {
+      if (!playlist.shots) playlist.shots = []
       state.playlistMap[playlist.id] = playlist
     })
+  },
+
+  [LOAD_PLAYLIST_START] (state) {
+  },
+
+  [LOAD_PLAYLIST_ERROR] (state) {
+  },
+
+  [LOAD_PLAYLIST_END] (state) {
   },
 
   [EDIT_PLAYLIST_START] (state, data) {
@@ -127,7 +159,7 @@ const mutations = {
       Object.assign(playlist, newPlaylist)
     } else {
       state.playlists.push(newPlaylist)
-      state.playlists = sortByName(state.playlists)
+      state.playlists = sortPlaylists(state.playlists)
     }
     state.playlistMap[newPlaylist.id] = newPlaylist
   },
@@ -142,11 +174,34 @@ const mutations = {
     )
     state.playlists.splice(playlistToDeleteIndex, 1)
     delete state.playlistMap[playlistToDelete.id]
+  },
 
-    state.deletePlaylist = {
-      isLoading: false,
-      isError: false
-    }
+  [LOAD_SHOT_PREVIEW_FILES_END] (state, { playlist, shot, previewFiles }) {
+    const shotToChange = playlist.shots.find(
+      (playlistShot) => playlistShot.shot_id === shot.id)
+    shotToChange.preview_files = previewFiles
+  },
+
+  [ADD_SHOT_TO_PLAYLIST] (state, {playlist, shot}) {
+    if (!playlist.shots) playlist.shots = []
+    playlist.shots.push({
+      shot_id: shot.id,
+      preview_file_id: shot.preview_file_id,
+      preview_files: shot.preview_files
+    })
+  },
+
+  [REMOVE_SHOT_FROM_PLAYLIST] (state, {playlist, shot}) {
+    if (!playlist.shots) playlist.shots = []
+    const shotPlaylistToDeleteIndex = playlist.shots.findIndex(
+      (shotPlaylist) => shotPlaylist.shot_id === shot.id
+    )
+    playlist.shots.splice(shotPlaylistToDeleteIndex, 1)
+  },
+
+  [CHANGE_PLAYLIST_PREVIEW] (state, { playlist, shotId, previewFileId }) {
+    const shotToChange = playlist.shots.find((shot) => shot.shot_id === shotId)
+    shotToChange.preview_file_id = previewFileId
   },
 
   [RESET_ALL] (state) {
