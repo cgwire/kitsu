06e75a15616137cf6e87296a57a4b22674825f81
Frank Rousseau <frank.rousseau@free.fr>
2018-03-28 19:13:54 +0200

    Allow ButtonLink widget to have no text

--- src/components/widgets/ButtonLink.vue
+++ src/components/widgets/ButtonLink.vue
@@ -6,12 +6,42 @@
   <span class="icon is-small" v-if="icon.length > 0 && icon.startsWith('fa')">
     <i :class="'fa ' + icon"></i>
   </span>
-  <plus-icon class="icon is-small" v-if="icon === 'plus'"></plus-icon>
-  <download-icon class="icon is-small" v-if="icon === 'download'"></download-icon>
-  <upload-icon class="icon is-small" v-if="icon === 'upload'"></upload-icon>
-  <Edit-icon class="icon is-small" v-if="icon === 'edit'"></edit-icon>
-  <trash-icon class="icon is-small" v-if="icon === 'delete'"></trash-icon>
-  <rotate-ccw-icon class="icon is-small" v-if="icon === 'restore'"></rotate-ccw-icon>
+  <plus-icon
+    :class="{
+      icon: true,
+      'is-small': true,
+      'only-icon': onlyIcon
+    }" v-if="icon === 'plus'"></plus-icon>
+  <download-icon
+    :class="{
+      icon: true,
+      'is-small': true,
+      'only-icon': onlyIcon
+    }" v-if="icon === 'download'"></download-icon>
+  <upload-icon
+    :class="{
+      icon: true,
+      'is-small': true,
+      'only-icon': onlyIcon
+    }" v-if="icon === 'upload'"></upload-icon>
+  <edit-icon
+    :class="{
+      icon: true,
+      'is-small': true,
+      'only-icon': onlyIcon
+    }" v-if="icon === 'edit'"></edit-icon>
+  <trash-icon
+    :class="{
+      icon: true,
+      'is-small': true,
+      'only-icon': onlyIcon
+    }" v-if="icon === 'delete'"></trash-icon>
+  <rotate-ccw-icon
+    :class="{
+      icon: true,
+      'is-small': true,
+      'only-icon': onlyIcon
+    }" v-if="icon === 'restore'"></rotate-ccw-icon>
 
   <span
     :class="{
@@ -59,9 +89,15 @@ export default {
       default: false,
       type: Boolean
     }
+  },
+  computed: {
+    onlyIcon: () => !this.text || this.text.length === 0
   }
 }
 </script>
 
 <style scoped>
+.button .icon.is-small.only-icon {
+  margin-right: 0;
+}
 </style>
