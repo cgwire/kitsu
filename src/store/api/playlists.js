420cda51273dd81b2d5eeff07400d23d31cc21c6
Frank Rousseau <frank.rousseau@free.fr>
2018-03-28 19:23:43 +0200

    Add functions to get details about preview files

--- src/store/api/playlists.js
+++ src/store/api/playlists.js
@@ -5,6 +5,16 @@ export default {
     client.get(`/api/data/projects/${production.id}/playlists`, callback)
   },
 
+  getPlaylist (production, playlist, callback) {
+    const path = `/api/data/projects/${production.id}/playlists/${playlist.id}`
+    client.get(path, callback)
+  },
+
+  getShotPreviewFiles (shot, callback) {
+    const path = `/api/data/shots/${shot.id}/preview-files`
+    client.get(path, callback)
+  },
+
   newPlaylist (playlist, callback) {
     const data = {
       name: playlist.name,
@@ -17,6 +27,10 @@ export default {
     const data = {
       name: playlist.name
     }
+    if (playlist.shots) {
+      data.shots = playlist.shots
+    }
+
     client.put(`/api/data/playlists/${playlist.id}`, data, callback)
   },
 
