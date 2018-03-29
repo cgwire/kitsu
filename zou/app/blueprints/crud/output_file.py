2ed8376e86a468c7e1746257af2c12431d3cddef
Frank Rousseau <frank.rousseau@free.fr>
2018-03-29 12:11:05 +0200

    Allow any user with related task to read an output file

--- zou/app/blueprints/crud/output_file.py
+++ zou/app/blueprints/crud/output_file.py
@@ -1,4 +1,6 @@
 from zou.app.models.output_file import OutputFile
+from zou.app.services import user_service
+
 from .base import BaseModelsResource, BaseModelResource
 
 
@@ -12,3 +14,6 @@ class OutputFileResource(BaseModelResource):
 
     def __init__(self):
         BaseModelResource.__init__(self, OutputFile)
+
+    def check_read_permissions(self, instance):
+        return user_service.check_project_access(instance.project_id)
