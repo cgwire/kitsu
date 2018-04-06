7c5b2cee63213835e7adb078aefb7d859767b8e0
Frank Rousseau <frank.rousseau@free.fr>
2018-04-06 16:15:34 +0200

    Allow to enable/disable job queue from config

--- zou/app/stores/queue_store.py
+++ zou/app/stores/queue_store.py
@@ -6,19 +6,20 @@ from zou.app import config
 
 
 try:
-    queue_store = redis.StrictRedis(
-        host=config.KEY_VALUE_STORE["host"],
-        port=config.KEY_VALUE_STORE["port"],
-        db=config.KV_EVENTS_JOB_INDEX,
-        decode_responses=True
-    )
-    queue_store.get(None)
+    if config.ENABLE_JOB_QUEUE:
+        queue_store = redis.StrictRedis(
+            host=config.KEY_VALUE_STORE["host"],
+            port=config.KEY_VALUE_STORE["port"],
+            db=config.KV_JOB_DB_INDEX,
+            decode_responses=True
+        )
+        queue_store.get(None)
 except redis.ConnectionError:
     try:
         import fakeredis
         revoked_tokens_store = fakeredis.FakeStrictRedis()
     except:
-        print("Cannot access to the required Redis instance")
         sys.exit(1)
 
-job_queue = Queue(connection=queue_store)
+if config.ENABLE_JOB_QUEUE:
+    job_queue = Queue(connection=queue_store)
--- zou/job_settings.py
+++ zou/job_settings.py
@@ -2,4 +2,4 @@ from zou.app import config
 
 REDIS_HOST = config.KEY_VALUE_STORE["host"]
 REDIS_PORT = config.KEY_VALUE_STORE["port"]
-REDIS_DB = config.KV_EVENTS_JOB_INDEX
+REDIS_DB = config.KV_JOB_DB_INDEX
