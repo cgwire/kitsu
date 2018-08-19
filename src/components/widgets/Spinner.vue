607ddc063bfc97d1b281451a298f711fac59e747
Frank Rousseau <frank.rousseau@free.fr>
2018-08-19 23:27:44 +0200

    Allow to change spinner size

--- src/components/widgets/Spinner.vue
+++ src/components/widgets/Spinner.vue
@@ -1,8 +1,12 @@
 <template>
-<div class="has-text-centered" v-if="isWhite">
+<div
+  class="has-text-centered"
+  :style="style"
+  v-if="isWhite"
+>
   <img src="../../assets/spinner-white.svg">
 </div>
-<div class="has-text-centered" v-else>
+<div class="has-text-centered" :style="style" v-else>
   <img src="../../assets/spinner.svg">
 </div>
 </template>
@@ -10,10 +14,22 @@
 <script>
 export default {
   name: 'spinner',
+
+  data () {
+    return {
+      style: {
+        width: this.size ? this.size + 'px' : 'auto'
+      }
+    }
+  },
+
   props: {
     isWhite: {
       type: Boolean,
       default: false
+    },
+    size: {
+      type: Number
     }
   }
 }
