20674808935c01ef5ded657317620b86860424aa
Frank Rousseau <frank.rousseau@free.fr>
2018-09-18 15:44:48 +0200

    Fix duplicated productions after avatar upload

--- src/components/modals/AddPreviewModal.vue
+++ src/components/modals/AddPreviewModal.vue
@@ -20,7 +20,7 @@
 
       <file-upload
         ref="preview-field"
-        accept=".png,.jpg,.mp4,.mov,.obj,.pdf,.ma,.mb"
+        accept=".png,.jpg,.mp4,.mov,.obj,.pdf,.ma,.mb,.zip,.rar,.jpeg"
         @fileselected="onFileSelected"
       ></file-upload>
 
