a3bd023e28b7881767df839cf41b6ef0b2311629
Frank Rousseau <frank.rousseau@free.fr>
2018-10-05 19:19:47 +0200

    Fix asset edition when no episode is linked

--- src/store/api/assets.js
+++ src/store/api/assets.js
@@ -40,8 +40,10 @@ export default {
       name: asset.name,
       description: asset.description,
       entity_type_id: asset.entity_type_id,
-      project_id: asset.project_id,
-      source_id: asset.source_id === 'null' ? null : asset.source_id
+      project_id: asset.project_id
+    }
+    if (asset.source_id === 'null' && asset.source_id) {
+      data.source_id = asset.source_id
     }
     client.put(`/api/data/entities/${asset.id}`, data, callback)
   },
