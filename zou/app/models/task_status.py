d53c292a1bf0679eafc841a367a43f5a301dc9ed
Frank Rousseau <frank.rousseau@free.fr>
2018-12-03 15:59:27 +0100

    Add allow to cg artist flag on task status
    It only has UI purpose for the moment.

--- zou/app/models/task_status.py
+++ zou/app/models/task_status.py
@@ -12,6 +12,9 @@ class TaskStatus(db.Model, BaseMixin, SerializerMixin):
     short_name = \
         db.Column(db.String(10), unique=True, nullable=False, index=True)
     color = db.Column(db.String(7), nullable=False)
+
     is_reviewable = db.Column(db.Boolean(), default=False)
     is_done = db.Column(db.Boolean(), default=False, index=True)
+    is_artist_allowed = db.Column(db.Boolean(), default=True)
+
     shotgun_id = db.Column(db.Integer)
