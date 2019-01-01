da1f344179a447a5909c2a451fd8e286280dd24b
Frank Rousseau <frank.rousseau@free.fr>
2019-01-01 18:22:43 +0100

    Mutualize selection code of asset and shot list

--- src/components/lists/AssetList.vue
+++ src/components/lists/AssetList.vue
@@ -248,19 +248,6 @@ export default {
     ValidationCell
   },
 
-  created () {
-    this.initHiddenColumns(this.validationColumns, this.hiddenColumns)
-  },
-
-  mounted () {
-    this.resizeHeaders()
-    window.addEventListener('keydown', this.onKeyDown, false)
-  },
-
-  beforeDestroy () {
-    window.removeEventListener('keydown', this.onKeyDown)
-  },
-
   computed: {
     ...mapGetters([
       'assets',
@@ -314,102 +301,6 @@ export default {
     ...mapActions([
       'displayMoreAssets'
     ]),
-
-    showHeaderMenu (columnId, event) {
-      const headerMenuEl = this.$refs.headerMenu.$el
-      if (headerMenuEl.className === 'header-menu') {
-        headerMenuEl.className = 'header-menu hidden'
-      } else {
-        headerMenuEl.className = 'header-menu'
-        let headerElement = event.srcElement.parentNode.parentNode
-        if (headerElement.tagName !== 'TH') {
-          headerElement = headerElement.parentNode
-        }
-        const left = headerElement.getBoundingClientRect().left + 1
-        const top = headerElement.getBoundingClientRect().bottom
-        headerMenuEl.style.left = left + 'px'
-        headerMenuEl.style.top = top + 'px'
-      }
-      this.lastHeaderMenuDisplayed = columnId
-    },
-
-    onMinimizeColumnToggled () {
-      this.hideColumn(this.lastHeaderMenuDisplayed)
-      this.showHeaderMenu()
-    },
-
-    onDeleteAllTasksClicked () {
-      this.$emit('delete-all-tasks', this.lastHeaderMenuDisplayed)
-      this.showHeaderMenu()
-    },
-
-    onTaskSelected (validationInfo) {
-      const selection = []
-      if (validationInfo.isShiftKey) {
-        if (this.lastSelection) {
-          let startX = this.lastSelection.x
-          let endX = validationInfo.x
-          let startY = this.lastSelection.y
-          let endY = validationInfo.y
-          if (validationInfo.x < this.lastSelection.x) {
-            startX = validationInfo.x
-            endX = this.lastSelection.x
-          }
-          if (validationInfo.y < this.lastSelection.y) {
-            startY = validationInfo.y
-            endY = this.lastSelection.y
-          }
-
-          for (let i = startX; i <= endX; i++) {
-            for (let j = startY; j <= endY; j++) {
-              const ref = 'validation-' + i + '-' + j
-              const validationCell = this.$refs[ref][0]
-              if (!this.assetSelectionGrid[i][j]) {
-                selection.push({
-                  entity: validationCell.entity,
-                  column: validationCell.column,
-                  task: validationCell.task,
-                  x: validationCell.rowX,
-                  y: validationCell.columnY
-                })
-              }
-            }
-          }
-          this.$store.commit('ADD_SELECTED_TASK', validationInfo)
-        }
-      } else if (!validationInfo.isCtrlKey) {
-        this.$store.commit('CLEAR_SELECTED_TASKS')
-      }
-      if (selection.length === 0) {
-        this.$store.commit('ADD_SELECTED_TASK', validationInfo)
-      } else {
-        this.$store.commit('ADD_SELECTED_TASKS', selection)
-      }
-
-      if (!validationInfo.isShiftKey && validationInfo.isUserClick) {
-        const x = validationInfo.x
-        const y = validationInfo.y
-        this.lastSelection = { x, y }
-        const ref = 'validation-' + x + '-' + y
-        const validationCell = this.$refs[ref][0]
-        this.$nextTick(() => {
-          this.scrollToValidationCell(validationCell)
-        })
-      }
-    },
-
-    onTaskUnselected (validationInfo) {
-      if (!validationInfo.isCtrlKey) {
-        if (this.nbSelectedTasks === 1) {
-          this.$store.commit('REMOVE_SELECTED_TASK', validationInfo)
-        } else {
-          this.$store.commit('CLEAR_SELECTED_TASKS')
-          this.$store.commit('ADD_SELECTED_TASK', validationInfo)
-        }
-      } else {
-        this.$store.commit('REMOVE_SELECTED_TASK', validationInfo)
-      }
-    },
 
     onBodyScroll (event, position) {
       this.$refs.headerWrapper.style.left = `-${position.scrollLeft}px`
@@ -517,33 +408,6 @@ export default {
             `${descriptionWidth}px`
         }
       }
-    },
-
-    onKeyDown (event) {
-      const lastSelection =
-        this.lastSelection ? this.lastSelection : { x: 0, y: 0 }
-      const i = lastSelection.x
-      const j = lastSelection.y
-      let validationCell = null
-      if (event.ctrlKey) {
-        if (event.keyCode === 37) {
-          validationCell = this.select(i, j - 1)
-        } else if (event.keyCode === 38) {
-          validationCell = this.select(i - 1, j)
-        } else if (event.keyCode === 39) {
-          validationCell = this.select(i, j + 1)
-        } else if (event.keyCode === 40) {
-          validationCell = this.select(i + 1, j)
-        }
-        this.scrollToValidationCell(validationCell)
-      }
-    },
-
-    select (i, j) {
-      const ref = 'validation-' + i + '-' + j
-      const validationCell = this.$refs[ref]
-      if (validationCell) validationCell[0].$el.click()
-      return validationCell ? validationCell[0] : 0
     }
   },
 
--- src/components/lists/base.js
+++ src/components/lists/base.js
@@ -2,6 +2,20 @@ import colors from '../../lib/colors'
 import Vue from 'vue'
 
 export const entityListMixin = {
+
+  created () {
+    this.initHiddenColumns(this.validationColumns, this.hiddenColumns)
+  },
+
+  mounted () {
+    this.resizeHeaders()
+    window.addEventListener('keydown', this.onKeyDown, false)
+  },
+
+  beforeDestroy () {
+    window.removeEventListener('keydown', this.onKeyDown)
+  },
+
   computed: {
     sortedValidationColumns () {
       let columns = [...this.validationColumns]
@@ -111,6 +125,135 @@ export const entityListMixin = {
         'border-left': `1px solid ${taskType.color}`,
         'background': this.getBackground(taskType.color)
       }
+    },
+
+    onTaskSelected (validationInfo) {
+      const selection = []
+      if (validationInfo.isShiftKey) {
+        if (this.lastSelection) {
+          let startX = this.lastSelection.x
+          let endX = validationInfo.x
+          let startY = this.lastSelection.y
+          let endY = validationInfo.y
+          let grid = this.assetSelectionGrid
+          if (!grid) grid = this.shotSelectionGrid
+
+          if (validationInfo.x < this.lastSelection.x) {
+            startX = validationInfo.x
+            endX = this.lastSelection.x
+          }
+          if (validationInfo.y < this.lastSelection.y) {
+            startY = validationInfo.y
+            endY = this.lastSelection.y
+          }
+
+          for (let i = startX; i <= endX; i++) {
+            for (let j = startY; j <= endY; j++) {
+              const ref = 'validation-' + i + '-' + j
+              const validationCell = this.$refs[ref][0]
+              if (!grid[i][j]) {
+                selection.push({
+                  entity: validationCell.entity,
+                  column: validationCell.column,
+                  task: validationCell.task,
+                  x: validationCell.rowX,
+                  y: validationCell.columnY
+                })
+              }
+            }
+          }
+          this.$store.commit('ADD_SELECTED_TASK', validationInfo)
+        }
+      } else if (!validationInfo.isCtrlKey) {
+        this.$store.commit('CLEAR_SELECTED_TASKS')
+      }
+      if (selection.length === 0) {
+        this.$store.commit('ADD_SELECTED_TASK', validationInfo)
+      } else {
+        this.$store.commit('ADD_SELECTED_TASKS', selection)
+      }
+
+      if (!validationInfo.isShiftKey && validationInfo.isUserClick) {
+        const x = validationInfo.x
+        const y = validationInfo.y
+        this.lastSelection = { x, y }
+        const ref = 'validation-' + x + '-' + y
+        const validationCell = this.$refs[ref][0]
+        this.$nextTick(() => {
+          this.scrollToValidationCell(validationCell)
+        })
+      }
+    },
+
+    onTaskUnselected (validationInfo) {
+      if (!validationInfo.isCtrlKey) {
+        if (this.nbSelectedTasks === 1) {
+          this.$store.commit('REMOVE_SELECTED_TASK', validationInfo)
+        } else {
+          this.$store.commit('CLEAR_SELECTED_TASKS')
+          this.$store.commit('ADD_SELECTED_TASK', validationInfo)
+        }
+      } else {
+        this.$store.commit('REMOVE_SELECTED_TASK', validationInfo)
+      }
+    },
+
+    onKeyDown (event) {
+      const lastSelection =
+        this.lastSelection ? this.lastSelection : { x: 0, y: 0 }
+      const i = lastSelection.x
+      const j = lastSelection.y
+      let validationCell = null
+      if (event.ctrlKey) {
+        if (event.keyCode === 37) {
+          validationCell = this.select(i, j - 1)
+        } else if (event.keyCode === 38) {
+          validationCell = this.select(i - 1, j)
+        } else if (event.keyCode === 39) {
+          validationCell = this.select(i, j + 1)
+        } else if (event.keyCode === 40) {
+          validationCell = this.select(i + 1, j)
+        }
+        this.scrollToValidationCell(validationCell)
+      }
+    },
+
+    select (i, j) {
+      const ref = 'validation-' + i + '-' + j
+      const validationCell = this.$refs[ref]
+      if (validationCell) validationCell[0].$el.click()
+      return validationCell ? validationCell[0] : 0
+    },
+
+    showHeaderMenu (columnId, event) {
+      const headerMenuEl = this.$refs.headerMenu.$el
+      if (headerMenuEl.className === 'header-menu') {
+        headerMenuEl.className = 'header-menu hidden'
+      } else {
+        headerMenuEl.className = 'header-menu'
+        let headerElement = event.srcElement.parentNode.parentNode
+        if (headerElement.tagName !== 'TH') {
+          headerElement = headerElement.parentNode
+        }
+        const headerBox = headerElement.getBoundingClientRect()
+        const left = headerBox.left + 11
+        const top = headerBox.bottom + 10
+        const width = headerBox.width - 1
+        headerMenuEl.style.left = left + 'px'
+        headerMenuEl.style.top = top + 'px'
+        headerMenuEl.style.width = width + 'px'
+      }
+      this.lastHeaderMenuDisplayed = columnId
+    },
+
+    onMinimizeColumnToggled () {
+      this.hideColumn(this.lastHeaderMenuDisplayed)
+      this.showHeaderMenu()
+    },
+
+    onDeleteAllTasksClicked () {
+      this.$emit('delete-all-tasks', this.lastHeaderMenuDisplayed)
+      this.showHeaderMenu()
     }
   }
 }
