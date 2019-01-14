96b275baeb471f16b46e215f73c4ddd264d36adb
Frank Rousseau <frank.rousseau@free.fr>
2019-01-14 13:58:57 +0100

    Use ffmpeg-python instead of moviepy
    FFMPEG python is much more performant. It doesn't add anything, which removes
    the special chars problem (moviepy doesn't handle special chars in the
    metadata).
    
    NB: It doesn't handle the dependency download. FFMPEG must be installed during
    the installation.

--- zou/app/utils/movie_utils.py
+++ zou/app/utils/movie_utils.py
@@ -1,4 +1,7 @@
 import os
+import math
+import ffmpeg
+from PIL import Image
 
 from moviepy.editor import VideoFileClip
 
@@ -18,19 +21,33 @@ def save_file(tmp_folder, instance_id, file_to_save):
 def generate_thumbnail(movie_path):
     """
     Generate a thumbnail to represent the movie given at movie path. It
-    takes a picture at the middle of the movie.
+    takes a picture at the first frame of the movie.
     """
     folder_path = os.path.dirname(movie_path)
     file_source_name = os.path.basename(movie_path)
     file_target_name = "%s.png" % file_source_name[:-4]
     file_target_path = os.path.join(folder_path, file_target_name)
 
-    movie_clip = VideoFileClip(movie_path)
-    movie_clip.save_frame(file_target_path, 0)
+    ffmpeg \
+        .input(movie_path, ss="00:00:00") \
+        .output(file_target_path, vframes=1) \
+        .run(
+            quiet=True
+        )
     return file_target_path
 
 
-def normalize_movie(movie_path, height=1080, bitrate="28M"):
+def get_movie_size(movie_path):
+    """
+    Returns movie resolution (extract a frame and returns its size).
+    """
+    image_path = generate_thumbnail(movie_path)
+    im = Image.open(image_path)
+    im.close()
+    return im.size
+
+
+def normalize_movie(movie_path):
     """
     Turn movie in a 1080p movie file.
     """
@@ -39,10 +56,27 @@ def normalize_movie(movie_path, height=1080, bitrate="28M"):
     file_target_name = "%s.mp4" % file_source_name[:-8]
     file_target_path = os.path.join(folder_path, file_target_name)
 
-    movie_clip = VideoFileClip(movie_path)
-    movie_clip = movie_clip.resize(height=height)
-    movie_clip.write_videofile(
-        file_target_path,
-        bitrate=bitrate
-    )
+    (width, h) = get_movie_size(movie_path)
+    resize_factor = width / h
+    height = 1080
+    width = math.floor(resize_factor * height)
+    if width % 2 == 1:
+        width = width + 1
+
+    ffmpeg \
+        .input(movie_path) \
+        .output(
+          file_target_path,
+          pix_fmt='yuv420p',
+          format="mp4",
+          r="24.00",
+          b="28M",
+          preset="medium",
+          vcodec="libx264",
+          s="%sx%s" % (width, height)
+        ) \
+        .run(
+            quiet=True
+        )
+
     return file_target_path
