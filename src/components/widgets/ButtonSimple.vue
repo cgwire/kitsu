c7ec90a751b508dbd6d984430a30b5689117dfd8
Frank Rousseau <frank.rousseau@free.fr>
2019-01-15 23:11:36 +0100

    Add a button widget
    To add easily a button with an icon.

---
+++ src/components/widgets/ButtonSimple.vue
@@ -0,0 +1,101 @@
+<template>
+<button
+  class="button"
+  @click="$emit('click')"
+>
+  <plus-icon
+    :class="{
+      icon: true,
+      'is-small': true,
+      'only-icon': !isText
+    }" v-if="icon === 'plus'" />
+  <download-icon
+    :class="{
+      icon: true,
+      'is-small': true,
+      'only-icon': !isText
+    }" v-if="icon === 'download'" />
+  <upload-icon
+    :class="{
+      icon: true,
+      'is-small': true,
+      'only-icon': !isText
+    }" v-if="icon === 'upload'" />
+  <edit-icon
+    :class="{
+      icon: true,
+      'is-small': true,
+      'only-icon': !isText
+    }" v-if="icon === 'edit'" />
+  <trash-icon
+    :class="{
+      icon: true,
+      'is-small': true,
+      'only-icon': !isText
+    }" v-if="icon === 'delete'" />
+  <rotate-ccw-icon
+    :class="{
+      icon: true,
+      'is-small': true,
+      'only-icon': !isText
+    }" v-if="icon === 'restore'" />
+
+  <span
+    :class="{
+      text: true,
+      'is-hidden-touch': isResponsive
+    }"
+    v-if="isText"
+  >
+    {{ text }}
+  </span>
+</button>
+</template>
+
+<script>
+import {
+  DownloadIcon,
+  EditIcon,
+  PlusIcon,
+  RotateCcwIcon,
+  TrashIcon,
+  UploadIcon
+} from 'vue-feather-icons'
+
+export default {
+  name: 'button-simple',
+  components: {
+    DownloadIcon,
+    EditIcon,
+    PlusIcon,
+    RotateCcwIcon,
+    TrashIcon,
+    UploadIcon
+  },
+  props: {
+    icon: {
+      default: '',
+      type: String
+    },
+    isResponsive: {
+      default: false,
+      type: Boolean
+    },
+    text: {
+      default: '',
+      type: String
+    },
+  },
+  computed: {
+    isText () {
+      return this.text && this.text.length > 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+.button .icon.is-small.only-icon {
+  margin-right: 0;
+}
+</style>
