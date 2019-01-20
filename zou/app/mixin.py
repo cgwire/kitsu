6d5a8601436c976832b48b3d26d5e5d224200227
Frank Rousseau <frank.rousseau@free.fr>
2019-01-20 17:31:27 +0100

    Allow args mixin to handle arrays

--- zou/app/mixin.py
+++ zou/app/mixin.py
@@ -4,9 +4,19 @@ from flask_restful import reqparse
 class ArgsMixin(object):
     def get_args(self, descriptors):
         parser = reqparse.RequestParser()
-        for (name, default, required) in descriptors:
-            if required is None:
-                required = False
-            parser.add_argument(name, required=required, default=default)
+        for descriptor in descriptors:
+            action = None
+
+            if len(descriptor) == 4:
+                (name, default, required, action) = descriptor
+            else:
+                (name, default, required) = descriptor
+
+            parser.add_argument(
+                name,
+                required=required,
+                default=default,
+                action=action
+            )
 
         return parser.parse_args()
