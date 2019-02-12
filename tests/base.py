afef79a22c3ebd0710da0f2dc3f77bec8b48c308
Frank Rousseau <frank.rousseau@free.fr>
2019-02-12 01:08:32 +0100

    Fix previous commit

--- tests/base.py
+++ tests/base.py
@@ -122,7 +122,7 @@ class ApiTestCase(unittest.TestCase):
             headers=self.post_headers
         )
         if (response.status_code == 500):
-            print(response.text)
+            print(response.data)
         self.assertEqual(response.status_code, code)
         return json.loads(response.data.decode("utf-8"))
 
