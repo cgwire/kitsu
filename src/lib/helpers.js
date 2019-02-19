736a4e0d29fea438ae8d1b6cfb3f909366174c82
Frank Rousseau <frank.rousseau@free.fr>
2019-02-19 18:48:57 +0100

    Add helpers for slugifying strings

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -247,3 +247,18 @@ export const getEntityPath = (entityId, productionId, section, episodeId) => {
 
   return route
 }
+
+export const slugify = (str) => {
+  str = str.replace(/^\s+|\s+$/g, '')
+    .toLowerCase()
+
+  const from = 'àáäâèéëêìíïîòóöôùúüûñç·/_,:'
+  const to = 'aaaaeeeeiiiioooouuuunc------'
+  for (let i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i))
+  }
+
+  return str.replace(/[^a-z0-9 -]/g, '')
+    .replace(/\s+/g, '_')
+    .replace(/-+/g, '_')
+}
