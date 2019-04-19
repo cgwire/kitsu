c1fddb4b694c476382503b5202ab82906ad7e41d
Frank Rousseau <frank.rousseau@free.fr>
2019-04-19 13:06:30 +0200

    Make new password error message more explicit

--- src/locales/en.js
+++ src/locales/en.js
@@ -108,7 +108,7 @@ export default {
     login_failed: 'Log in failed, please verify your credentials',
     login_page: 'Cancel',
     reset_change_password: 'Change password',
-    reset_change_password_form_failed: 'There is a problem with the password you gave. Please, verify that it is more than 6 chars long and that both passwords match.',
+    reset_change_password_form_failed: 'There is a problem with the password you gave. Please, verify that it is at least 7 chars long and that both passwords match.',
     reset_change_password_failed: 'Changing password failed. Please, restart the whole procedure again.',
     reset_change_password_succeed: 'Your password was changed successfully. Please, go back to the login page to use it.',
     reset_change_password_title: 'Enter a new password',
