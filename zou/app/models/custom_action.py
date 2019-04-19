9aa48c8fe237bb4f7aeba75dbee0cf92f6c7c803
Frank Rousseau <frank.rousseau@free.fr>
2019-04-19 12:09:59 +0200

    Add is AJAX column to custom actions

--- zou/app/models/custom_action.py
+++ zou/app/models/custom_action.py
@@ -12,3 +12,4 @@ class CustomAction(db.Model, BaseMixin, SerializerMixin):
     name = db.Column(db.String(80), nullable=False)
     url = db.Column(db.String(400))
     entity_type = db.Column(db.String(40), default="all")
+    is_ajax = db.Column(db.Boolean(), default=False)
