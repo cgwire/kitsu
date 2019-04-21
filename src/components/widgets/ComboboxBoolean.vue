6b4429bf9036cfd07cbe86131ce4ba0b6d436515
Frank Rousseau <frank.rousseau@free.fr>
2019-04-21 18:18:01 +0200

    Add combobox widgets for booleans and models

---
+++ src/components/widgets/ComboboxBoolean.vue
@@ -0,0 +1,69 @@
+<template>
+<combobox
+
+  :label="label"
+  :disabled="disabled"
+  :options="booleanOptions"
+  :value="value"
+  @input="emitValue"
+  @enter="emitEnter"
+/>
+</template>
+
+<script>
+import Combobox from '../widgets/Combobox'
+
+export default {
+  name: 'combobox-boolean',
+
+  components: {
+    Combobox
+  },
+
+  props: {
+    label: {
+      default: '',
+      type: String
+    },
+
+    value: {
+      default: 'false',
+      type: String
+    },
+
+    options: {
+      default: () => [],
+      type: Array
+    },
+
+    disabled: {
+      default: false,
+      type: Boolean
+    }
+  },
+
+  data () {
+    return {
+      booleanOptions: [
+        {label: this.$t('main.yes'), value: 'true'},
+        {label: this.$t('main.no'), value: 'false'}
+      ]
+    }
+  },
+
+  computed: {
+  },
+
+  methods: {
+    emitValue (value) {
+      this.$emit('input', value)
+    },
+    emitEnter (value) {
+      this.$emit('enter', value)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
