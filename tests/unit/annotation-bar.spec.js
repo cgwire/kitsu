ffb4a0d1c7a6acda0872c0ea3d254009493bb33f
Frank Rousseau <frank.rousseau@free.fr>
2019-05-05 13:25:01 +0200

    Add tests for annotation bar widget

--- tests/unit/annotation-bar.spec.js
+++ tests/unit/annotation-bar.spec.js
@@ -2,8 +2,32 @@ import { mount } from '@vue/test-utils'
 import AnnotationBar from '../../src/components/pages/playlists/AnnotationBar'
 
 describe('AnnotationBar', () => {
-  const wrapper = mount(AnnotationBar)
-  test('should mount for testing', () => {
-    expect(1).toEqual(1)
+  const wrapper = mount(AnnotationBar, {
+    propsData: {
+      annotations: [
+        { time: 10 },
+        { time: 20 },
+      ],
+      maxDurationRaw: 100,
+      width: 100
+    }
+  })
+
+  describe('Mount', () => {
+    test('Ensure marks are there', () => {
+      console.log('toto')
+      const marks = wrapper.findAll('.annotation-mark')
+      expect(marks.length).toEqual(2)
+      expect(marks.at(0).element.style.left).toEqual('7px')
+    })
+  })
+
+  describe('Methods', () => {
+    test('getAnnotationPosition', () => {
+      let position = wrapper.vm.getAnnotationPosition({ time: 10 })
+      expect(position).toEqual(7)
+      position = wrapper.vm.getAnnotationPosition({ time: 20 })
+      expect(position).toEqual(17)
+    })
   })
 })
