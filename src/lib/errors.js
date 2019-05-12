f23c21016ac64576228a549ef01d12a0e8c2ca06
Frank Rousseau <frank.rousseau@free.fr>
2019-05-12 18:10:38 +0200

    Simplify forced logout
    On 401, there is no need to reset the application since the page is reloaded.

--- src/lib/errors.js
+++ src/lib/errors.js
@@ -1,14 +1,8 @@
-import store from '../store'
-import {
-  RESET_ALL,
-  USER_LOGOUT
-} from '../store/mutation-types.js'
-
 const errors = {
   backToLogin () {
-    store.commit(RESET_ALL)
-    store.commit(USER_LOGOUT)
-    window.location.replace('/login')
+    if (window.location !== '/login') {
+      window.location.replace('/login')
+    }
   }
 }
 export default errors
