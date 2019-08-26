30419a532ad8bb8e003fa2fc48fe1dcfdc79a8bb
Frank Rousseau <frank.rousseau@free.fr>
2019-08-26 11:10:41 +0200

    Add news CRUD routes

---
+++ zou/app/blueprints/crud/news.py
@@ -0,0 +1,15 @@
+from zou.app.models.news import News
+
+from .base import BaseModelResource, BaseModelsResource
+
+
+class NewssResource(BaseModelsResource):
+
+    def __init__(self):
+        BaseModelsResource.__init__(self, News)
+
+
+class NewsResource(BaseModelResource):
+
+    def __init__(self):
+        BaseModelResource.__init__(self, News)
