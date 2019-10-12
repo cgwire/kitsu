9533421e2fb406fa3e746203cf6bc7153a90792b
Frank Rousseau <frank.rousseau@free.fr>
2019-10-12 15:53:37 +0200

    Add German an Spanish locales

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,9 +1,13 @@
 import en from './en'
+import es from './es'
+import de from './de'
 import fr from './fr'
 import zh from './zh'
 
 export default {
+  de: de,
   en: en,
+  es: es,
   zh: zh,
   fr: fr
 }
