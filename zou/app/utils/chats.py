b363b0ffc9e4fd7790f418f84107c3b7233642f1
Frank Rousseau <frank.rousseau@free.fr>
2019-11-06 18:52:12 +0100

    Allow to format messages sent to Slack

--- zou/app/utils/chats.py
+++ zou/app/utils/chats.py
@@ -3,7 +3,16 @@ from slackclient import SlackClient
 
 def send_to_slack(app_token, userid, message):
     client = SlackClient(token=app_token)
+    blocks = [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text":  message,
+            }
+        },
+    ]
     client.api_call(
-        "chat.postMessage", channel="@%s" % userid, text=message, as_user=True
+        "chat.postMessage", channel="@%s" % userid, blocks=blocks, as_user=True
     )
     return True
