64e484df690707e979b649d93080fa13f04e8ce9
GoOz <gooz@foojin.com>
2020-01-10 18:07:25 +0100

    Update Quota page with store data

--- src/components/pages/quota/Quota.vue
+++ src/components/pages/quota/Quota.vue
@@ -1,219 +1,212 @@
 <template>
   <div class="quota">
-    <div class="quota-fixed">
+    <div class="quota-fixed" v-if="this.quotaLength > 0 && !isLoading">
       <div class="quota-header quota-row">
-        <div class="header-cell row-cell row-cell--name">Name</div>
-        <div class="header-cell row-cell row-cell--weekly">Quota per week</div>
-        <div class="header-cell row-cell row-cell--average">Average</div>
+        <div class="header-cell row-cell row-cell--name">
+          {{ $t('quota.name') }}
+        </div>
+        <div class="header-cell row-cell row-cell--weekly">
+          {{ $t('quota.quota_week') }}
+        </div>
+        <div class="header-cell row-cell row-cell--average">
+          {{ $t('quota.average') }}
+        </div>
       </div>
-      <div class="quota-row" v-for="(user, i) in users" :key="user.name + '-' + i">
-        <div class="row-cell row-cell--name">{{ user.name }}</div>
-        <div class="row-cell row-cell--weekly">{{ user.weeklyQuota }}</div>
-        <div class="row-cell row-cell--average">{{ user.averageQuota }}</div>
+      <div class="quota-row" v-for="(people, key, i) in quotaMap" :key="'-' + i">
+        <div class="row-cell row-cell--name">
+          <people-avatar :size="30" :person="personMap[key]"/>
+          {{ personMap[key].full_name }}
+        </div>
+        <div class="row-cell row-cell--weekly">{{ '-' }}</div>
+        <div class="row-cell row-cell--average">{{ '-' }}</div>
       </div>
     </div>
-    <div class="quota-scrolled">
+    <div class="quota-scrolled" v-if="this.quotaLength > 0 && !isLoading">
       <div class="quota-header quota-row">
-        <div class="header-cell row-cell">1</div>
-        <div class="header-cell row-cell">2</div>
-        <div class="header-cell row-cell">3</div>
-        <div class="header-cell row-cell">4</div>
-        <div class="header-cell row-cell">5</div>
-        <div class="header-cell row-cell">6</div>
-        <div class="header-cell row-cell">7</div>
-        <div class="header-cell row-cell">8</div>
-        <div class="header-cell row-cell">9</div>
-        <div class="header-cell row-cell">10</div>
-        <div class="header-cell row-cell">11</div>
-        <div class="header-cell row-cell">12</div>
+        <div
+          class="header-cell row-cell"
+          v-for="month in monthRange"
+          :key="'month-' + month"
+          v-if="detailLevel === 'month'"
+        >
+          {{ monthToString(month) }}
+        </div>
+        <div
+          class="header-cell row-cell"
+          :key="'week-' + week"
+          v-for="week in weekRange"
+          v-if="detailLevel === 'week'"
+        >
+          {{ week }}
+        </div>
+
+        <div
+          class="header-cell row-cell"
+          :key="'day-' + day"
+          v-for="day in dayRange"
+          v-if="detailLevel === 'day'"
+        >
+          {{ day }}
+        </div>
       </div>
-      <div class="quota-row" v-for="(user, i) in users" :key="user.name + '-' + i">
-        <template v-for="(day, i) in user.days">
-          <div class="row-cell" :key="day.day + '-' + i">{{ day.quota }}</div>
-        </template>
+      <div class="quota-row" v-for="(people, key, i) in quotaMap" :key="'-' + i">
+        <div class="row-cell"
+          v-for="month in monthRange"
+          :key="'month-' + month"
+          v-if="detailLevel === 'month'"
+        >
+          {{ quotaMap[key][year+'-'+ month.toString().padStart(2, '0')] || '-'}}
+        </div>
+        <div class="row-cell"
+          v-for="week in weekRange"
+          :key="'week-' + week"
+          v-if="detailLevel === 'week'"
+        >
+          {{ quotaMap[key][year+'-'+week] || '-'}}
+        </div>
+        <div class="row-cell"
+          v-for="day in dayRange"
+          :key="'day-' + day"
+          v-if="detailLevel === 'day'"
+        >
+          {{ quotaMap[key][year+'-'+ month.toString().padStart(2, '0') +'-'+day] || '-'}}
+        </div>
       </div>
     </div>
+    <table-info
+      :is-loading="isLoading"
+      :is-error="isError"
+    />
   </div>
 </template>
 
 <script>
+
+import moment from 'moment-timezone'
+import { mapGetters, mapActions } from 'vuex'
+import PeopleAvatar from '../../widgets/PeopleAvatar'
+import TableInfo from '../../widgets/TableInfo'
+import {
+  monthToString,
+  getMonthRange,
+  getWeekRange,
+  getDayRange
+} from '../../../lib/time'
+
 export default {
   name: 'quota',
-  mounted () {
-    console.debug('mounted!')
+  components: {
+    PeopleAvatar,
+    TableInfo
   },
-  data () {
-    return {
-      users: {
-        first: {
-          name: 'Alice Cooper',
-          weeklyQuota: 25,
-          averageQuota: 23,
-          days: {
-            one: {
-              day: 1,
-              quota: 24
-            },
-            second: {
-              day: 2,
-              quota: 23
-            },
-            third: {
-              day: 3,
-              quota: 22
-            },
-            fourth: {
-              day: 1,
-              quota: 24
-            },
-            fifth: {
-              day: 2,
-              quota: 23
-            },
-            sixth: {
-              day: 3,
-              quota: 22
-            },
-            seventh: {
-              day: 1,
-              quota: 24
-            },
-            eighth: {
-              day: 2,
-              quota: 23
-            },
-            nineth: {
-              day: 3,
-              quota: 22
-            },
-            tenth: {
-              day: 1,
-              quota: 24
-            },
-            eleventh: {
-              day: 2,
-              quota: 23
-            },
-            twelveth: {
-              day: 3,
-              quota: 22
-            }
-          }
-        },
-        second: {
-          name: 'Ann Kennedy',
-          weeklyQuota: 26,
-          averageQuota: 24,
-          days: {
-            one: {
-              day: 1,
-              quota: 22
-            },
-            second: {
-              day: 2,
-              quota: 23
-            },
-            third: {
-              day: 3,
-              quota: 22
-            },
-            fourth: {
-              day: 1,
-              quota: 24
-            },
-            fifth: {
-              day: 2,
-              quota: 23
-            },
-            sixth: {
-              day: 3,
-              quota: 22
-            },
-            seventh: {
-              day: 1,
-              quota: 24
-            },
-            eighth: {
-              day: 2,
-              quota: 23
-            },
-            nineth: {
-              day: 3,
-              quota: 22
-            },
-            tenth: {
-              day: 1,
-              quota: 24
-            },
-            eleventh: {
-              day: 2,
-              quota: 23
-            },
-            twelveth: {
-              day: 3,
-              quota: 22
-            }
-          }
-        },
-        third: {
-          name: 'Brenan Mason',
-          weeklyQuota: 24,
-          averageQuota: 20,
-          days: {
-            one: {
-              day: 1,
-              quota: 21
-            },
-            second: {
-              day: 2,
-              quota: 23
-            },
-            third: {
-              day: 3,
-              quota: 22
-            },
-            fourth: {
-              day: 1,
-              quota: 24
-            },
-            fifth: {
-              day: 2,
-              quota: 23
-            },
-            sixth: {
-              day: 3,
-              quota: 22
-            },
-            seventh: {
-              day: 1,
-              quota: 24
-            },
-            eighth: {
-              day: 2,
-              quota: 23
-            },
-            nineth: {
-              day: 3,
-              quota: 22
-            },
-            tenth: {
-              day: 1,
-              quota: 24
-            },
-            eleventh: {
-              day: 2,
-              quota: 23
-            },
-            twelveth: {
-              day: 3,
-              quota: 22
-            }
-          }
-        }
-      }
+
+  mounted () {
+    this.loadData()
+  },
+
+  props: {
+    taskTypeId: {
+      type: String,
+      required: true
+    },
+    detailLevel: {
+      type: String,
+      default: 'day',
+      required: true
+    },
+    year: {
+      type: Number,
+      default: 0
+    },
+    month: {
+      type: Number,
+      default: 0
+    },
+    week: {
+      type: Number,
+      default: 0
+    },
+    day: {
+      type: Number,
+      default: 0
     }
   },
-  methods () {}
+
+  data () {
+    return {
+      isLoading: false,
+      isError: false,
+      quotaMap: {},
+      quotaLength: 0,
+      currentMonth: moment().month() + 1,
+      currentYear: moment().year(),
+      currentWeek: moment().week()
+    }
+  },
+
+  computed: {
+    ...mapGetters([
+      'shotMap',
+      'personMap'
+    ]),
+
+    monthRange () {
+      return getMonthRange(this.year, this.currentYear, this.currentMonth)
+    },
+
+    dayRange () {
+      return getDayRange(
+        this.year,
+        this.month,
+        this.currentYear,
+        this.currentMonth
+      )
+    },
+
+    weekRange () {
+      return getWeekRange(this.year, this.currentYear, this.currentWeek)
+    }
+  },
+
+  methods: {
+    ...mapActions([
+      'loadShots',
+      'computeQuota'
+    ]),
+    loadData () {
+      this.loadShots((err) => {
+        this.isLoading = true
+        if (err) {
+          console.error(err)
+        } else {
+          if (this.taskTypeId) {
+            this.computeQuota({
+              taskTypeId: this.taskTypeId,
+              detailLevel: this.detailLevel
+            })
+              .then(quotas => {
+                this.quotaMap = quotas
+                this.isLoading = false
+                this.quotaLength = Object.keys(this.quotaMap).length
+                console.debug('method', this.quotaMap)
+              })
+          }
+        }
+      })
+    },
+    monthToString
+  },
+
+  watch: {
+    taskTypeId () {
+      if (this.taskTypeId) {
+        this.loadData()
+      }
+    },
+    detailLevel () {
+      this.loadData()
+    }
+  }
 }
 </script>
 
@@ -223,11 +216,10 @@ export default {
       color: $white;
     }
     .row-cell {
-      border-bottom-color: $dark-grey-lightest;
+      border-bottom-color: $dark-grey-light;
     }
     .quota-row:nth-child(odd):not(.quota-header) .row-cell {
-      background-color: $light-grey-light;
-      color: $dark-grey;
+      background-color: $dark-grey-lightmore;
     }
   }
   .quota {
@@ -255,21 +247,35 @@ export default {
     flex-wrap: nowrap;
   }
   .row-cell {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
     width: 4rem;
     padding: 1rem;
-    border-bottom: 1px solid $dark-grey;
+    height: 4rem;
+    max-height: 4rem;
+    border-bottom: 1px solid $light-grey-light;
     flex: 0 0 auto;
     overflow: hidden;
     text-overflow: ellipsis;
     white-space: nowrap;
-    text-align: center;
+  }
+  .quota-row:nth-child(odd):not(.quota-header) .row-cell {
+    background-color: $white-grey-light;
   }
   .quota-row:last-child .row-cell {
     border: 0;
   }
+  .header-cell {
+    font-weight: bold;
+  }
   .row-cell--name {
     width: 12rem;
-    text-align: left;
+    justify-content: flex-start;
+    .avatar {
+      margin-right: .5rem;
+    }
   }
   .row-cell--weekly {
     width: 10rem;
