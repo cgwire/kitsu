f1ee45b5491b4dee60fe1df2294570ce1644cb81
GoOz <gooz@foojin.com>
2020-03-20 11:36:46 +0100

    fix: new Tasktype priority

--- src/components/modals/EditTaskTypeModal.vue
+++ src/components/modals/EditTaskTypeModal.vue
@@ -8,7 +8,7 @@
 
     <div class="box">
 
-      <h1 class="title" v-if="isEditing()">
+      <h1 class="title" v-if="isEditing">
         {{ $t("task_types.edit_title") }} {{ taskTypeToEdit.name }}
       </h1>
       <h1 class="title" v-else>
@@ -125,7 +125,10 @@ export default {
     ...mapGetters([
       'taskTypes',
       'taskTypeStatusOptions'
-    ])
+    ]),
+    isEditing () {
+      return this.taskTypeToEdit && this.taskTypeToEdit.id
+    }
   },
 
   methods: {
@@ -145,10 +148,6 @@ export default {
         this.form.priority = this.newPriority(this.form.for_shots)
       }
       this.$emit('confirm', this.form)
-    },
-
-    isEditing () {
-      return this.taskTypeToEdit && this.taskTypeToEdit.id
     }
   }
 }
