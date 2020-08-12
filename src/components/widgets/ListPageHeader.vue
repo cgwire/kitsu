d9987f2f08be462fcd7df76c0113f3dd9ca71591
Frank Rousseau <frank.rousseau@free.fr>
2020-08-12 19:26:40 +0200

    [QA] Add component to simplify list page headers

---
+++ src/components/widgets/ListPageHeader.vue
@@ -0,0 +1,57 @@
+<template>
+<div class="level page-header">
+  <div class="level-left">
+    <page-title :text="title" />
+  </div>
+  <div class="level-right">
+    <button-simple
+      class="level-item"
+      icon="plus"
+      :text="newEntryLabel"
+      @click="$emit('new-clicked')"
+    />
+  </div>
+</div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+import ButtonSimple from '../widgets/ButtonSimple'
+import PageTitle from '../widgets/PageTitle'
+
+export default {
+  name: 'list-page-header',
+
+  components: {
+    ButtonSimple,
+    PageTitle
+  },
+
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    newEntryLabel: {
+      type: String,
+      default: ''
+    }
+  },
+
+  computed: {
+    ...mapGetters([
+    ])
+  },
+
+  methods: {
+    ...mapActions([
+    ])
+  },
+
+  watch: {}
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
