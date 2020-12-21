b6c7a6844e3f92e52aca712c825a2026b33c1b32
Frank Rousseau <frank.rousseau@free.fr>
2020-12-21 12:21:21 +0100

    [productions] Fix display of action buttons for closed productions

--- src/components/lists/ProductionList.vue
+++ src/components/lists/ProductionList.vue
@@ -89,7 +89,7 @@
           <td class="resolution">
             {{ entry.resolution }}
           </td>
-          <row-actions
+          <row-actions-cell
             :entry-id="entry.id"
             @edit-clicked="$emit('edit-clicked', entry)"
             @delete-clicked="$emit('delete-clicked', entry)"
