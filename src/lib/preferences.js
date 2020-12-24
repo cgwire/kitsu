7e948a8a8269df01e41b1a99da4332f5b5dff75e
Frank Rousseau <frank.rousseau@free.fr>
2020-12-24 12:34:55 +0100

    [qa] Add a library to manage local preferences

---
+++ src/lib/preferences.js
@@ -0,0 +1,15 @@
+export default {
+
+  setPreference (key, item) {
+    return localStorage.setItem(key, item)
+  },
+
+  getPreference (key) {
+    return localStorage.getItem(key)
+  },
+
+  getBoolPreference (key) {
+    const item = this.getPreference(key)
+    return item === 'true'
+  }
+}
