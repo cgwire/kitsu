8e186eeb94ef35860d9ff8eede082609153baae6
Frank Rousseau <frank.rousseau@free.fr>
2021-01-19 19:54:00 +0100

    [widget] Textarea emit ctrlenter event
    Useful to confirm an edition.

--- src/components/widgets/TextareaField.vue
+++ src/components/widgets/TextareaField.vue
@@ -1,16 +1,16 @@
 <template>
 <div class="field">
-  <label class="label">{{ label }}</label>
+  <label class="label" v-if="label">{{ label }}</label>
   <p class="control">
     <textarea
+      ref="input"
       class="input"
       :class="'input ' + inputClass"
-      ref="input"
       :placeholder="placeholder"
       :type="type"
       :value="value"
-      @input="updateValue()"
-      @keyup.enter="emitEnter()"
+      @input="updateValue"
+      @keyup.enter="emitEnter"
       @keyup="emitKeyup"
     />
   </p>
@@ -61,14 +61,16 @@ export default {
       this.$emit('keyup', event)
     },
     focus () {
-      this.$refs.input.focus()
+      if (this.$refs.input) this.$refs.input.focus()
     }
   }
 }
 </script>
 <style lang="scss" scoped>
 textarea {
+  height: 100%;
   min-height: 8em;
+  max-height: 100%;
   padding: 0.5em;
 }
 </style>
