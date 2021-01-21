2ab0dd4d33bc7fcef1555a28a9b7924eab4b6af5
Frank Rousseau <frank.rousseau@free.fr>
2021-01-21 15:09:05 +0100

    [ux] Add a stylier spinner

--- src/components/widgets/Spinner.vue
+++ src/components/widgets/Spinner.vue
@@ -2,21 +2,36 @@
 <div
   class="has-text-centered"
   :style="sizeStyle"
-  v-if="isWhite"
 >
-  <img src="../../assets/spinner-white.svg">
-</div>
-<div class="has-text-centered" :style="sizeStyle" v-else>
-  <img src="../../assets/spinner.svg">
+  <img src="../../assets/spinner-white.svg" v-if="isWhite" />
+  <square-grid :style="{ margin: 'auto'}" v-else-if="isProcessing"/>
+  <origami :style="{ margin: 'auto'}" v-else-if="isStylish" />
+  <img src="../../assets/spinner.svg" v-else>
 </div>
 </template>
 
 <script>
+import {
+  SquareGrid,
+  Origami
+} from 'vue-loading-spinner'
 export default {
   name: 'spinner',
+  components: {
+    SquareGrid,
+    Origami
+  },
 
   props: {
     isWhite: {
+      type: Boolean,
+      default: false
+    },
+    isProcessing: {
+      type: Boolean,
+      default: false
+    },
+    isStylish: {
       type: Boolean,
       default: false
     },
