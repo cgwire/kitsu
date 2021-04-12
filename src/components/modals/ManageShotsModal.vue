dad06ffa505c8948e92e3fe4ad0b1686d3b5e964
Frank Rousseau <frank.rousseau@free.fr>
2021-04-12 21:15:20 +0200

    [qa] Remove debug log

--- src/components/modals/ManageShotsModal.vue
+++ src/components/modals/ManageShotsModal.vue
@@ -79,7 +79,7 @@
               @keyup.tab="focusAddShot"
               @click="selectSequence(sequence.id)"
               v-for="sequence in displayedSequences">
-              {{ sequence.name }} toto
+              {{ sequence.name }}
             </div>
           </div>
           <div class="field">
