71f8a6b01a12afc367bb2df84047300893ce98fa
Frank Rousseau <frank.rousseau@free.fr>
2021-04-21 19:38:23 +0200

    [qa] Add tests

---
+++ tests/unit/lib/render.spec.js
@@ -0,0 +1,23 @@
+import { getTaskTypeStyle, renderComment } from '@/lib/render'
+
+describe('render', () => {
+  test('getTaskTypeStyle', () => {
+    const task = { task_type_color: 'red' }
+    expect(getTaskTypeStyle(task)).toEqual({
+      'border-left': '4px solid red'
+    })
+    expect(getTaskTypeStyle(null)).toEqual({
+      'border-left': '4px solid transparent'
+    })
+  })
+
+  test('renderComment', () => {
+    const input = "Text @Jhon Doe"
+    const mentions = ['person-1']
+    const personMap = { 'person-1': { id: 'person-1', full_name: 'Jhon Doe' } }
+    const result = renderComment(input, mentions, personMap)
+    expect(result).toEqual(
+      '<p>Text <a class="mention" href="/people/person-1">@Jhon Doe</a></p>'
+    )
+  })
+})
