0826003f69bcd3bbf83aba9a620949844489e161
Frank Rousseau <frank.rousseau@free.fr>
2021-08-19 18:15:23 +0200

    [previews] Allow to upload Storyboard files

--- src/lib/files.js
+++ src/lib/files.js
@@ -2,10 +2,10 @@ export default {
   IMG_EXTENSIONS: ['png', 'jpg', 'jpeg', 'gif', 'svg'],
   ALL_EXTENSIONS_STRING: '.png,.jpg,.mp4,.mov,.obj,.pdf,.ma,.mb,.zip,' +
     '.rar,.jpeg,.svg,.blend,.wmv,.m4v,.ai,.comp,.exr,.psd,.hip,.gif,.ae,' +
-    '.fla,.flv,.swf',
+    '.fla,.flv,.swf,.sbbkp',
   ALL_EXTENSIONS: [
     'png', 'jpg', 'mp4', 'mov', 'obj', 'pdf', 'ma', 'mb', 'zip', 'rar',
     'jpeg', 'svg', 'blend', 'wmv', 'm4v', 'ai', 'comp', 'exr', 'psd', 'hip',
-    'gif', 'ae', 'fla', 'flv', 'swf'
+    'gif', 'ae', 'fla', 'flv', 'swf', 'sbbkp'
   ]
 }
