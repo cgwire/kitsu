ac2fe1ab45e0a5aeb5966b55b8ed746ac146dd90
Frank Rousseau <frank.rousseau@free.fr>
2021-09-15 08:52:20 +0200

    [qa] Up Node.js version for Travis CI

---
+++ src/components/widgets/BigThumbnailsButton.vue
@@ -0,0 +1,70 @@
+<template>
+<button
+  :class="{
+    'level-item': true,
+    button: true,
+    'is-toggle': true,
+    'is-on': isBigThumbnails
+  }"
+  :title="$t(isBigThumbnails ? 'tasks.small_thumbnails' : 'tasks.big_thumbnails')"
+  @click="toggleBigThumbnails"
+>
+  <grid-icon class="icon is-small" />
+</button>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import {
+  GridIcon
+} from 'vue-feather-icons'
+
+export default {
+  name: 'big-thumbnails-button',
+  components: {
+    GridIcon
+  },
+
+  props: {
+  },
+
+  computed: {
+    ...mapGetters([
+      'isBigThumbnails'
+    ])
+  },
+
+  methods: {
+    ...mapActions([
+      'setBigThumbnails',
+      'setSmallThumbnails'
+    ]),
+
+    toggleBigThumbnails () {
+      if (this.isBigThumbnails) {
+        this.setSmallThumbnails()
+      } else {
+        this.setBigThumbnails()
+      }
+    }
+  },
+
+  mounted () {
+    if (localStorage.getItem('big-thumbnails') === 'false') {
+      this.setSmallThumbnails()
+    } else {
+      this.setBigThumbnails()
+    }
+  },
+
+  watch: {
+    isBigThumbnails () {
+      localStorage.setItem(
+        'big-thumbnails',
+        this.isBigThumbnails,
+        { expires: '1M' }
+      )
+    }
+  }
+}
+</script>
