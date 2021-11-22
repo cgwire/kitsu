8fadece21597b095dc40b577852fec480b67f68e
Frank Rousseau <frank.rousseau@free.fr>
2021-11-22 17:40:00 +0100

    [qa] Allow to publish a mp3 file

--- src/lib/files.js
+++ src/lib/files.js
@@ -2,7 +2,7 @@ export default {
   IMG_EXTENSIONS: ['png', 'jpg', 'jpeg', 'gif', 'svg'],
   ALL_EXTENSIONS_STRING: '.png,.jpg,.mp4,.mov,.obj,.glb,.pdf,.ma,.mb,.zip,' +
     '.rar,.jpeg,.svg,.blend,.wmv,.m4v,.ai,.comp,.exr,.psd,.hip,.gif,.ae,' +
-    '.fla,.flv,.swf,.sbbkp,.wav.mp3',
+    '.fla,.flv,.swf,.sbbkp,.wav,.mp3',
   ALL_EXTENSIONS: [
     'png', 'jpg', 'mp4', 'mov', 'obj', 'glb', 'pdf', 'ma', 'mb', 'zip', 'rar',
     'jpeg', 'svg', 'blend', 'wmv', 'm4v', 'ai', 'comp', 'exr', 'psd', 'hip',
