7af59135fd98ac609429271e46f80faaaf4a82c8
Evan Blaudy <evan@cg-wire.com>
2022-05-05 11:56:11 +0200

    [translations] missing translation for tasks.small_thumbnails

--- src/locales/en.js
+++ src/locales/en.js
@@ -1006,6 +1006,7 @@ export default {
     back_to_list: 'back to list',
     bigger: 'Widen task panel',
     big_thumbnails: 'Show big thumbnails',
+    small_thumbnails: 'Show small thumbnails',
     change_status_to: 'Change task status to:',
     change_preview: 'Change preview',
     change_priority: 'Change priority to:',
