aa6610f3addd525c96256badc40fe6e162efb103
Frank Rousseau <frank.rousseau@free.fr>
2022-05-25 15:36:39 +0200

    [qa] Remove commented code

--- src/components/modals/AddThumbnailsModal.vue
+++ src/components/modals/AddThumbnailsModal.vue
@@ -258,39 +258,6 @@ export default {
         return window.URL.createObjectURL(form.get('file'))
       }
       return ''
-      /*
-      const canvas = document.createElement('img')
-      const img = document.createElement('img')
-      const reader = new FileReader()
-      reader.onload = (e) => { img.src = e.target.result }
-      reader.readAsDataURL(form.get('file'))
-
-      let ctx = canvas.getContext('2d')
-      ctx.drawImage(img, 0, 0)
-
-      const MAX_WIDTH = 50
-      const MAX_HEIGHT = 33
-      let width = img.width
-      let height = img.height
-
-      if (width > height) {
-        if (width > MAX_WIDTH) {
-          height *= MAX_WIDTH / width
-          width = MAX_WIDTH
-        }
-      } else {
-        if (height > MAX_HEIGHT) {
-          width *= MAX_HEIGHT / height
-          height = MAX_HEIGHT
-        }
-      }
-      canvas.width = width
-      canvas.height = height
-      ctx = canvas.getContext('2d')
-      ctx.drawImage(img, 0, 0, width, height)
-
-      return canvas.toDataURL('image/png')
-      */
     },
 
     markLoading (assetId) {
