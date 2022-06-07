bb3d538267a7c23afb378922f14d314c5451072f
Frank Rousseau <frank.rousseau@free.fr>
2022-06-07 16:14:20 +0200

    [tasks] Add draft library

---
+++ src/lib/drafts.js
@@ -0,0 +1,15 @@
+const drafts = {
+  setTaskDraft (taskId, text) {
+    return localStorage.setItem('draft-' + taskId, text)
+  },
+
+  getTaskDraft (taskId) {
+    return localStorage.getItem('draft-' + taskId)
+  },
+
+  clearTaskDraft (taskId) {
+    return localStorage.removeItem('draft-' + taskId)
+  }
+}
+
+export default drafts
