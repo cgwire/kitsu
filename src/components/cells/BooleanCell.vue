4bd94e70d6b693715f6431260c09c506e9e309f7
Frank Rousseau <frank.rousseau@free.fr>
2022-08-14 15:45:30 +0200

    [widgets] Add helpers for booleans
    Represention, cell and field.

---
+++ src/components/cells/BooleanCell.vue
@@ -0,0 +1,42 @@
+<template>
+<td class="has-text-centered">
+  <boolean-rep class="bool-icon" :value="value" />
+</td>
+</template>
+
+<script>
+import BooleanRep from '@/components/widgets/BooleanRep'
+
+export default {
+  name: 'boolean-cell',
+  components: {
+    BooleanRep
+  },
+
+  data () {
+    return {}
+  },
+
+  props: {
+    value: {
+      default: false,
+      type: Boolean
+    }
+  },
+
+  computed: {
+  },
+
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+td {
+  padding-top: 11px;
+  .bool-icon {
+    margin-top: 0px;
+  }
+}
+</style>
