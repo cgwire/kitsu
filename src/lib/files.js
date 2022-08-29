15fbafa392cfe3ec621c3af50cf81e3408240b7e
Evan Blaudy <evan@cg-wire.com>
2022-08-30 01:56:58 +0200

    [qa] add support for webm files in preview

--- src/lib/files.js
+++ src/lib/files.js
@@ -1,11 +1,14 @@
+const ALL_EXTENSIONS = [
+  'png', 'jpg', 'mp4', 'mov', 'obj', 'glb', 'pdf', 'ma', 'mb', 'zip', 'rar',
+  'jpeg', 'svg', 'blend', 'wmv', 'm4v', 'ai', 'comp', 'exr', 'psd', 'hip',
+  'gif', 'ae', 'fla', 'flv', 'swf', 'sbbkp', 'wav', 'mp3', 'webm'
+]
+
+const ALL_EXTENSIONS_STRING = ALL_EXTENSIONS.map(extension => '.' + extension)
+  .join(',')
+
 export default {
   IMG_EXTENSIONS: ['png', 'jpg', 'jpeg', 'gif', 'svg'],
-  ALL_EXTENSIONS_STRING: '.png,.jpg,.mp4,.mov,.obj,.glb,.pdf,.ma,.mb,.zip,' +
-    '.rar,.jpeg,.svg,.blend,.wmv,.m4v,.ai,.comp,.exr,.psd,.hip,.gif,.ae,' +
-    '.fla,.flv,.swf,.sbbkp,.wav,.mp3',
-  ALL_EXTENSIONS: [
-    'png', 'jpg', 'mp4', 'mov', 'obj', 'glb', 'pdf', 'ma', 'mb', 'zip', 'rar',
-    'jpeg', 'svg', 'blend', 'wmv', 'm4v', 'ai', 'comp', 'exr', 'psd', 'hip',
-    'gif', 'ae', 'fla', 'flv', 'swf', 'sbbkp', 'wav', 'mp3'
-  ]
+  ALL_EXTENSIONS_STRING,
+  ALL_EXTENSIONS
 }
