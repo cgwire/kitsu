6e1faa0b24f97c8bbc2221a14b27f467c13405df
Evan Blaudy <evan@cg-wire.com>
2022-09-21 15:33:17 +0200

    add avi files support for preview

--- src/lib/files.js
+++ src/lib/files.js
@@ -1,7 +1,7 @@
 const ALL_EXTENSIONS = [
   'png', 'jpg', 'mp4', 'mov', 'obj', 'glb', 'pdf', 'ma', 'mb', 'zip', 'rar',
   'jpeg', 'svg', 'blend', 'wmv', 'm4v', 'ai', 'comp', 'exr', 'psd', 'hip',
-  'gif', 'ae', 'fla', 'flv', 'swf', 'sbbkp', 'wav', 'mp3', 'webm'
+  'gif', 'ae', 'fla', 'flv', 'swf', 'sbbkp', 'wav', 'mp3', 'webm', 'avi'
 ]
 
 const ALL_EXTENSIONS_STRING = ALL_EXTENSIONS.map(extension => '.' + extension)
