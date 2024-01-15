bc10f99e4c0f767f986d9ed007b52a84895fa99d
Frank Rousseau <frank.rousseau@free.fr>
2024-01-15 01:29:45 +0100

    [calendar] Allow to select a task

--- src/components/widgets/UserCalendar.vue
+++ src/components/widgets/UserCalendar.vue
@@ -6,22 +6,40 @@
       :options="calendarOptions"
     >
       <template v-slot:eventContent="arg">
-        <b>{{ arg.timeText }}</b>
-        <i>{{ arg.event.title }}</i>
+        <div
+          class="flexrow pl1 calendar-event"
+          :style="{
+            background: arg.event.backgroundColor
+          }"
+          :title="arg.event.title"
+          @click="onEventClicked(arg.event, arg.event.extendedProps.task_id)"
+        >
+          <entity-thumbnail
+            class="flexrow-item mr1"
+            :preview-file-id="arg.event.extendedProps.preview_file_id"
+          />
+          <span class="flexrow-item event-title">
+            {{ arg.event.title }}
+          </span>
+        </div>
       </template>
     </full-calendar>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
 import FullCalendar from '@fullcalendar/vue'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import multiMonthPlugin from '@fullcalendar/multimonth'
+
+import EntityThumbnail from '@/components/widgets/EntityThumbnail'
 
 export default {
   name: 'user-calendar',
   components: {
+    EntityThumbnail,
     FullCalendar
   },
 
@@ -34,12 +52,13 @@ export default {
 
   data() {
     return {
+      currentTask: {},
       calendarOptions: {
-        plugins: [dayGridPlugin],
+        plugins: [dayGridPlugin, multiMonthPlugin],
         headerToolbar: {
           left: 'prev,next today',
           center: 'title',
-          right: 'dayGridMonth,dayGridWeek'
+          right: 'dayGridMonth,dayGridWeek,multiMonthYear'
         },
         initialView: 'dayGridMonth'
       }
@@ -51,11 +70,14 @@ export default {
   },
 
   computed: {
-    ...mapGetters(['taskTypeMap'])
+    ...mapGetters(['taskMap', 'taskTypeMap'])
   },
 
   methods: {
+    ...mapActions(['addSelectedTasks', 'clearSelectedTasks']),
+
     resetEvents() {
+      this.calendarEvents = []
       if (this.$refs.calendar) {
         const calendarApi = this.$refs.calendar.getApi()
         calendarApi.removeAllEvents()
@@ -64,15 +86,36 @@ export default {
           .map(task => {
             const taskType = this.taskTypeMap.get(task.task_type_id)
             return {
-              title: task.entity_name,
+              title: task.full_entity_name,
               start: task.start_date.substring(0, 10),
               end: task.due_date.substring(0, 10),
-              color: taskType.color
+              color: '#666',
+              borderColor: '#666',
+              backgroundColor: taskType.color,
+              preview_file_id: task.entity_preview_file_id,
+              selected: false,
+              task_status_id: task.task_status_id,
+              task_id: task.id
             }
           })
           .forEach(event => {
-            calendarApi.addEvent(event)
+            const ev = calendarApi.addEvent(event)
+            this.calendarEvents.push(ev)
           })
+      }
+    },
+
+    onEventClicked(event, taskId) {
+      const task = this.taskMap.get(taskId)
+      if (task === this.currentTask) {
+        this.currentTask = {}
+        this.clearSelectedTasks()
+        event.setProp('selected', false)
+      } else {
+        this.currentTask = task
+        this.clearSelectedTasks()
+        this.addSelectedTasks([{ task }])
+        event.setProp('extendedProps.selected', true)
       }
     }
   },
@@ -138,5 +181,14 @@ export default {
     border: 1px solid var(--border);
     color: var(--text);
   }
+}
+
+.calendar-event {
+  overflow: auto;
+  padding: 0.2em;
+}
+
+.event-title {
+  padding-left: 0.6em;
 }
 </style>
