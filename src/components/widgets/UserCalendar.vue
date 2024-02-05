3bdebc27c6e67170054bb8f4d30fd034898a0f6b
Nicolas Pennec <nicolas.pennec@cg-wire.com>
2024-02-05 14:31:33 +0100

    [widget] set Monday as first day of the week on the User Calendar

--- src/components/widgets/UserCalendar.vue
+++ src/components/widgets/UserCalendar.vue
@@ -60,7 +60,8 @@ export default {
           center: 'title',
           right: 'dayGridMonth,dayGridWeek,multiMonthYear'
         },
-        initialView: 'dayGridMonth'
+        initialView: 'dayGridMonth',
+        firstDay: 1
       }
     }
   },
