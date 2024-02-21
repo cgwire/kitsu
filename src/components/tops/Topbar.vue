64b2eb2454e4ca3a0ecce7f6ebc2a4245154e3aa
Nicolas Pennec <nicolas.pennec@cg-wire.com>
2024-02-21 11:51:05 +0100

    [nav] fix link to tvshow without episode

--- src/components/tops/Topbar.vue
+++ src/components/tops/Topbar.vue
@@ -408,7 +408,11 @@ export default {
       }
       if (production.production_type === 'tvshow') {
         route.name = `episode-${section}`
-        route.params.episode_id = production.first_episode_id
+        if (section !== 'edits' && production.first_episode_id) {
+          route.params.episode_id = production.first_episode_id
+        } else {
+          route.params.episode_id = 'all'
+        }
       }
       return route
     },
