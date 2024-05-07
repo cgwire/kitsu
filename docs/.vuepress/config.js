6772c73102ab6337772624a98107f8999e416d22
Nicolas Pennec <nicolas.pennec@cg-wire.com>
2024-05-07 12:24:21 +0200

    Fix vuepress config on invalid children

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,66 +2,48 @@ module.exports = {
   title: 'Kitsu Documentation',
   description: 'The documentation for Kitsu, the collaboration platform for Animation studios',
   head: [
-     ['link', { rel: 'shortcut icon', type: "image/x-icon", href: `./img/favicon.ico` }]
+    ['link', { rel: 'shortcut icon', type: 'image/x-icon', href: `./img/favicon.ico` }]
   ],
   themeConfig: {
-   sidebar: [{
-     title: 'Introductions to Kitsu Basics',
-     collapsable: true,
-     children: [
-      ['/', 'Introduction'],
-       '/configure-kitsu/',
-       '/team/'
-       ]
-   },
-    {
-     title: 'Create your production',
-     collapsable: true,
-     children: [
-	    '/short/',
-	    '/short-shot/',
-	    '/short-asset/',
-      '/tvshow/',
-      '/feature/',
-      '/videogame/',
-      '/nft/'
-	 ]
-   },
-   	{
-     title: 'Assignation, Estimation and schedule',
-     collapsable: false,
-     children: [
-	    '/assignation/',
-	    '/estimation/',
-	    '/schedules/'
-     ]
-	},
-   {
-     title: 'Status, Publish, Thumbnail and Review',
-     collapsable: false,
-     children: [
-	    '/status/',
-	    '/publish/',
-	    '/review/'
-     ]
-   },
-	{
-     title: 'Developper',
-     collapsable: false,
-     children: [
-      '/custom-actions/',
-      '/publisher/',
-	    '/chat-integration/',
-	    '/installation/'
-     ]
-	},	
-    {
-     title: 'Producer',
-     collapsable: false,
-     children: [
-       '/installation/',
-       '/development-environment/'
-     ]
-    }]
+    sidebar: [
+      {
+        title: 'Introductions to Kitsu Basics',
+        collapsable: true,
+        children: [['/', 'Introduction'], '/configure-kitsu/', '/team/']
+      },
+      {
+        title: 'Create your production',
+        collapsable: true,
+        children: [
+          '/short/',
+          '/short-shot/',
+          '/short-asset/',
+          '/tvshow/',
+          '/feature/',
+          '/videogame/',
+          '/nft/'
+        ]
+      },
+      {
+        title: 'Assignation, Estimation and schedule',
+        collapsable: false,
+        children: ['/assignation/', '/estimation/', '/schedules/']
+      },
+      {
+        title: 'Status, Publish, Thumbnail and Review',
+        collapsable: false,
+        children: ['/status/', '/publish/', '/review/']
+      },
+      {
+        title: 'Producer',
+        collapsable: false,
+        children: ['/custom-actions/', '/publisher/', '/chat-integration/']
+      },
+      {
+        title: 'Developer',
+        collapsable: false,
+        children: ['/installation/']
+      }
+    ]
   }
 }
