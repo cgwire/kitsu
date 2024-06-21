2ccc307c47542269758eb165d3ac52dc7c42b661
scott <scott@jammedia.ie>
2024-06-21 15:34:39 +0100

    AssetType set short_name on edit

--- src/components/modals/EditAssetTypeModal.vue
+++ src/components/modals/EditAssetTypeModal.vue
@@ -128,6 +128,7 @@ export default {
     return {
       form: {
         name: '',
+        short_name: '',
         task_types: []
       }
     }
@@ -198,12 +199,14 @@ export default {
         const types = this.assetTypeToEdit.task_types || []
         this.form = {
           name: this.assetTypeToEdit.name,
+          short_name: this.assetTypeToEdit.short_name,
           task_types: [...types],
           archived: String(this.assetTypeToEdit.archived === true)
         }
       } else {
         this.form = {
           name: '',
+          short_name: '',
           task_types: [],
           archived: 'false'
         }
