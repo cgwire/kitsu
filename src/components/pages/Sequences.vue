6f57b5766973488817d4445ebb0eb3721e7766a4
Nicolas Pennec <nicolas.pennec@cg-wire.com>
2024-07-17 10:44:32 +0200

    [sequences] Fix an edge case when loading sequences

--- src/components/pages/Sequences.vue
+++ src/components/pages/Sequences.vue
@@ -652,7 +652,7 @@ export default {
     currentSection() {
       if (
         (this.isTVSHow && this.displayedSequences.length === 0) ||
-        this.displayedSequences[0].episode_id !== this.currentEpisode.id
+        this.displayedSequences[0]?.episode_id !== this.currentEpisode?.id
       ) {
         this.$refs['sequence-search-field'].setValue('')
         this.$store.commit('SET_SEQUENCE_LIST_SCROLL_POSITION', 0)
