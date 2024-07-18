3d5ee6a4b3d590ce4bcd820b09150eb6715951b6
Nicolas Pennec <nicolas.pennec@cg-wire.com>
2024-07-18 16:57:04 +0200

    [vue3] refactor set locale

--- src/router/routes.js
+++ src/router/routes.js
@@ -118,7 +118,7 @@ export const routes = [
           next(nextPath)
         } else {
           timezone.setTimezone()
-          lang.setLocale()
+          lang.setLocale(userStore.state.user.locale)
           if (store.state.productions.openProductions.length === 0) {
             init(err => {
               if (err) {
@@ -155,7 +155,7 @@ export const routes = [
           next(nextPath)
         } else {
           timezone.setTimezone()
-          lang.setLocale()
+          lang.setLocale(userStore.state.user.locale)
           const isProhibited =
             !userStore.getters.isCurrentUserAdmin(userStore.state) &&
             to &&
