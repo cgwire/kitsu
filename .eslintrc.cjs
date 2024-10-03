1ea49f0e6756da890ac554b1f76f78b684d776d3
Nicolas Pennec <nicolas.pennec@cg-wire.com>
2024-10-03 23:31:07 +0200

    [vue3] update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:vue/recommended',
+    'plugin:vue/vue3-recommended',
     'plugin:prettier/recommended'
   ],
   rules: {
@@ -26,14 +26,15 @@ module.exports = {
     // additional rules for Vue
     'vue/component-definition-name-casing': ['error', 'kebab-case'],
     'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+    'vue/custom-event-name-casing': ['error', 'kebab-case'],
     'vue/eqeqeq': ['error', 'always', { null: 'ignore' }],
+    'vue/no-unused-emit-declarations': 'error',
     'vue/prop-name-casing': ['error', 'camelCase'],
+    'vue/require-explicit-emits': 'error',
 
     // disabled vue/recommended rules
     'vue/attributes-order': 'off',
     'vue/multi-word-component-names': 'off',
-    'vue/no-lone-template': 'off',
-    'vue/no-use-v-if-with-v-for': 'off',
     'vue/no-v-html': 'off',
     'vue/order-in-components': 'off',
     'vue/require-default-prop': 'off',
