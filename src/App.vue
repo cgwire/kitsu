e680d960ca8c89a4fec9ede1d0fbea348415f970
Frank Rousseau <frank@cg-wire.com>
2024-10-21 21:09:18 +0200

    [conf] Adapt sentry config to vue 3

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,7 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import { getCurrentInstance } from 'vue'
 
 import PreviewModal from '@/components/modals/PreviewModal.vue'
 import Spinner from '@/components/widgets/Spinner.vue'
@@ -125,10 +126,17 @@ export default {
 
     setupSentry(config) {
       if (config.sentry?.dsn?.length) {
-        sentry.init(this.$router, {
-          dsn: config.sentry.dsn,
-          sampleRate: config.sentry.sampleRate
-        })
+        const instance = getCurrentInstance()
+        const app = instance.appContext.app
+
+        sentry.init(
+          app,
+          this.$router,
+          {
+            dsn: config.sentry.dsn,
+            sampleRate: config.sentry.sampleRate
+          }
+        )
       }
     }
   },
--- src/lib/sentry.js
+++ src/lib/sentry.js
@@ -1,12 +1,11 @@
-import Vue from 'vue'
 import * as Sentry from '@sentry/vue'
 
 import { name, version } from '@/../package.json'
 
 export default {
-  init(router, { dsn, sampleRate = 0.1 }) {
+  init(app, router, { dsn, sampleRate = 0.1 }) {
     Sentry.init({
-      Vue,
+      Vue: app,
       dsn,
       enabled: process.env.NODE_ENV === 'production',
       release: `${name}@${version}`,
