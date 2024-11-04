83130418da2678b5941f0116af1e2423efd8fb0b
Frank Rousseau <frank@cg-wire.com>
2024-11-04 13:03:45 +0100

    [vue3] Remove compat mode from vite config

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          compatConfig: {
-            MODE: 3
-          }
-        }
-      }
-    })
+    vue({})
   ],
   build: {
     sourcemap: true
