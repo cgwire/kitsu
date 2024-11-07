2097c39598cba33ab04776e111381e8591649d61
Frank Rousseau <frank@cg-wire.com>
2024-11-07 15:57:42 +0100

    [vue3] Fix Vite config for production build

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,15 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({})
-  ],
+  plugins: [vue({})],
   build: {
     sourcemap: true
   },
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     alias: {
-      '@': path.resolve(__dirname, 'src/')
+      '@': path.resolve(__dirname, 'src/'),
+      vue: 'vue/dist/vue.esm-bundler.js'
     }
   },
   css: {
