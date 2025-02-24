e3560eb3a9f76668870bf127f40de5ec72e8f7b6
Nicolas Pennec <nicolas.pennec@cg-wire.com>
2025-02-24 15:55:09 +0100

    [qa] fix edge case of undefined value

--- src/components/lists/TaskList.vue
+++ src/components/lists/TaskList.vue
@@ -737,6 +737,7 @@ export default {
     },
 
     onBodyScroll(event) {
+      if (this.$refs.body) return
       const position = event.target
       const maxHeight =
         this.$refs.body.scrollHeight - this.$refs.body.offsetHeight
