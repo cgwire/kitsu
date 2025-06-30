00f5e4d53d8ad7e7b9dcf257bb068a8f8535601d
Nicolas Pennec <nicolas.pennec@cg-wire.com>
2025-06-30 10:08:14 +0200

    [qa] fix edge case of undefined value

--- src/components/mixins/player.js
+++ src/components/mixins/player.js
@@ -559,7 +559,11 @@ export const playerMixin = {
             this.setAnnotationDrawingMode(true)
           }, 100)
         }
-        if (this.isPlaying && this.isPicture(entity.preview_file_extension)) {
+        if (
+          this.isPlaying &&
+          entity &&
+          this.isPicture(entity.preview_file_extension)
+        ) {
           this.playPicture()
         }
       }
