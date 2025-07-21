976a3b614b7d4f012d094781380ec78102ef5b31
Michael Delaporte <michael@pixelscoder.com>
2025-07-21 13:46:45 +0200

    Implement shape annotation drawing with rectangle and circle

--- src/components/widgets/ButtonSimple.vue
+++ src/components/widgets/ButtonSimple.vue
@@ -38,6 +38,9 @@
     <edit2-icon class="icon" v-else-if="icon === 'pencil'" />
     <layers-icon class="icon" v-else-if="icon === 'layers'" />
     <list-icon class="icon" v-else-if="icon === 'list'" />
+    <shapes-icon class="icon" v-else-if="icon === 'shapes'" />
+    <rectangle-horizontal-icon class="icon" v-else-if="icon === 'rectangle'" />
+    <circle-icon class="icon" v-else-if="icon === 'circle'" />
     <triangle-icon class="icon" v-else-if="icon === 'triangle'" />
     <music-icon class="icon" v-else-if="icon === 'music'" />
     <square-icon class="icon" v-else-if="icon === 'eraser'" />
@@ -75,6 +78,7 @@ import {
   ChevronDownIcon,
   ChevronLeftIcon,
   ChevronRightIcon,
+  CircleIcon,
   ClockIcon,
   CodepenIcon,
   CornerLeftDownIcon,
@@ -98,8 +102,10 @@ import {
   PaperclipIcon,
   PauseIcon,
   PlusIcon,
+  RectangleHorizontalIcon,
   SaveIcon,
   SendIcon,
+  ShapesIcon,
   SkipBackIcon,
   SkipForwardIcon,
   SquareIcon,
@@ -141,8 +147,11 @@ export default {
     PaperclipIcon,
     PauseIcon,
     PlusIcon,
+    RectangleHorizontalIcon,
+    CircleIcon,
     SaveIcon,
     SendIcon,
+    ShapesIcon,
     SkipBackIcon,
     SkipForwardIcon,
     SquareIcon,
