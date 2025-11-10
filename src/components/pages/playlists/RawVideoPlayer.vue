b17b62e5ddfcc754085131921fcaedaf444da514
Nicolas Pennec <nicolas.pennec@cg-wire.com>
2025-11-10 09:57:45 +0100

    [qa] fix edge case of undefined value

--- src/components/pages/playlists/RawVideoPlayer.vue
+++ src/components/pages/playlists/RawVideoPlayer.vue
@@ -394,7 +394,7 @@ export default {
         } else if (this.currentPlayer) {
           this.currentPlayer.src = ''
         }
-        if (nextEntity.preview_file_extension === 'mp4' && this.nextPlayer) {
+        if (nextEntity?.preview_file_extension === 'mp4' && this.nextPlayer) {
           this.nextPlayer.src = this.getMoviePath(nextEntity)
         } else if (this.nextPlayer) {
           this.nextPlayer.src = ''
