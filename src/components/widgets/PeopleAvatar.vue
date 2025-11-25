192d1a324225112ffcbdb155cfbc2c3133fa7957
Nicolas Pennec <nicolas.pennec@cg-wire.com>
2025-11-25 12:19:42 +0100

    [qa] fix edge case of undefined person property

--- src/components/widgets/PeopleAvatar.vue
+++ src/components/widgets/PeopleAvatar.vue
@@ -3,7 +3,7 @@
     class="avatar has-text-centered"
     :class="{ bot: person.is_bot }"
     :style="style"
-    v-if="isLink && person.id"
+    v-if="person?.id && isLink"
   >
     <router-link
       :to="{
@@ -29,7 +29,7 @@
     :class="{ bot: person.is_bot }"
     :title="person.full_name"
     :style="style"
-    v-else
+    v-else-if="person"
   >
     <img
       :loading="isLazy ? 'lazy' : undefined"
