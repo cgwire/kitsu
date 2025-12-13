3a2a5d2a52db8056e22acbf095c632858cc87c45
Nicolas Pennec <nicolas.pennec@cg-wire.com>
2025-12-13 16:00:27 +0100

    [qa] improve intersection function

--- src/lib/array.js
+++ src/lib/array.js
@@ -1,6 +1,11 @@
-/* Returns the intersection of several arrays */
+/**
+ * Finds the intersection of multiple arrays
+ * @param {Array<Array>} arrays - An array containing the arrays to compare
+ * @returns {Array} - An array containing only elements present in all arrays
+ */
 export const intersection = arrays => {
-  let [first, ...rest] = arrays
-  rest = rest.map(x => new Set(x))
-  return first.filter(x => rest.every(a => a.has(x)))
+  if (!arrays?.length) return []
+  const [first, ...rest] = arrays
+  const sets = rest.map(arr => new Set(arr))
+  return first.filter(item => sets.every(set => set.has(item)))
 }
