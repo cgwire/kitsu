2aaaa1ae23c00e45799578e19dcdc3c426c2081f
Nicolas Pennec <nicolas.pennec@cg-wire.com>
2026-03-13 13:07:19 +0100

    [qa] remove callback-based methods for cleaner API usage

--- src/store/api/client.js
+++ src/store/api/client.js
@@ -2,40 +2,6 @@ import superagent from 'superagent'
 import errors from '@/lib/errors'
 
 const client = {
-  get(path, callback) {
-    superagent.get(path).end((err, res) => {
-      if (res?.statusCode === 401) return errors.backToLogin()
-      callback(err, res?.body)
-    })
-  },
-
-  post(path, data, callback) {
-    superagent
-      .post(path)
-      .send(data)
-      .end((err, res) => {
-        if (res?.statusCode === 401) return errors.backToLogin()
-        callback(err, res?.body)
-      })
-  },
-
-  put(path, data, callback) {
-    superagent
-      .put(path)
-      .send(data)
-      .end((err, res) => {
-        if (res?.statusCode === 401) return errors.backToLogin()
-        callback(err, res?.body)
-      })
-  },
-
-  del(path, callback) {
-    superagent.del(path).end((err, res) => {
-      if (res?.statusCode === 401) return errors.backToLogin()
-      callback(err, res?.body)
-    })
-  },
-
   pget(path) {
     return superagent
       .get(path)
